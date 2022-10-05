6c5e0dbdb090417ef41ece6964341a5f9e2cce92
kabo87777 <brian891125@gmail.com>
2022-10-05 18:01:22 +0800

    feat(SideBar.vue):add dropdown and setting button

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -8,7 +8,17 @@
 			:modal="false"
 			class="mt-15"
 		>
-			<h3>Left Sidebar</h3>
+			<Dropdown
+				v-model="selectedProgram"
+				:options="programs"
+				optionLabel="program_name"
+				class="w-full h-60px !text-3xl tracking-2px"
+			/>
+			<Button
+				label="專案設定"
+				icon="pi pi-cog"
+				class="p-button-secondary p-button-text my-5 w-full text-left tracking-2px"
+			/>
 		</Sidebar>
 		<!-- <Button icon="pi pi-arrow-right" @click="visibleLeft = true"  /> -->
 	</div>
@@ -17,5 +27,12 @@
 <script setup lang="ts">
 import Sidebar from "primevue/sidebar";
 import { ref } from "vue";
+import Dropdown from "primevue/dropdown";
+import Button from "primevue/button";
+
 const visibleLeft = ref(true);
+const selectedProgram = ref({ program_name: "111年研究生審查" });
+const programs = ref([{ program_name: "111年研究生審查" }]);
 </script>
+
+<style></style>
