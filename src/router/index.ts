6ea919b70f6d6e69417863c84a90ad57e3a5541d
40947010S <iamtim303@gmail.com>
2022-10-07 20:55:36 +0800

    feat(index):build project-application-setting.vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,6 +11,12 @@ const routes: Array<RouteRecordRaw> = [
 		name: "home",
 		component: () => import("../views/home/HomeView.vue"),
 	},
+	{
+		path: "/project-application-setting",
+		name: "project-application-setting",
+		component: () =>
+			import("../views/home/project-application-setting.vue"),
+	},
 ];
 
 const router: Router = createRouter({
