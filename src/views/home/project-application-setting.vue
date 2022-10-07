3254409ac33aaa80b0843546a1a5b6cb49757d0b
40947010S <iamtim303@gmail.com>
2022-10-07 20:57:12 +0800

    feat(project-application-setting):build two botton

---
+++ src/views/home/project-application-setting.vue
@@ -0,0 +1,36 @@
+<template>
+	<!-- button by windi CSS -->
+	<div
+		class="flex items-center tracking-0.16 h-11.5 w-56 sans not-italic font-medium text-2xl leading-8 tracking-0.16 text-dark-900 flex-none order-0 grow-0"
+	>
+		申請上傳欄位
+	</div>
+	<div class="flex h-12">
+		<button
+			class="mx-2 bg-green-400 flex flex-row justify-center items-center w-35 h-11 rounded-lg flex-none order-1 grow-0"
+		>
+			<div
+				class="flex items-center h-30 w-10 sans not-italic font-normal text-xl leading-7 tracking-2 text-dark-900 flex-none order-1 grow-0"
+			>
+				設定儲存
+			</div>
+			<i
+				class="ml-4 mr-2 box-border text-sm text-dark-900 order-0 pi pi-check"
+			></i>
+		</button>
+		<button
+			class="flex flex-row justify-center items-center w-35 h-11 rounded-lg border-2 border-ntnuRed bg-white center flex-none order-0 grow-0"
+		>
+			<div
+				class="flex items-center h-30 w-10 sans not-italic font-normal text-xl leading-7 tracking-2 text-ntnuRed flex-none order-1 grow-0"
+			>
+				取消變更
+			</div>
+			<i
+				class="ml-4 mr-2 box-border text-sm text-ntnuRed order-0 pi pi-times"
+			></i>
+		</button>
+	</div>
+</template>
+<style></style>
+<script setup lang="ts"></script>
