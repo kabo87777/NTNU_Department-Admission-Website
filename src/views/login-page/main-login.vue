2827f314b08fc9bc508611f455ae105396e53e05
Birkhoff Lee <admin@birkhoff.me>
2022-10-07 03:49:23 +0000

    Feature: i18n support

--- src/views/login-page/main-login.vue
+++ src/views/login-page/main-login.vue
@@ -13,18 +13,25 @@
 					class="h-15 w-80 mt-150px"
 				/>
 				<div class="mt-150px ml-50px text-4xl font-bold text-gray-500">
-					資訊工程學系
+					{{ $t("資訊工程學系") }}
 				</div>
 			</div>
 			<div class="mt-100px ml-150px text-4xl font-bold text-gray-500">
-				資訊工程學系報名系統
+				{{ $t("資訊工程學系報名系統") }}
 			</div>
 			<div class="mt-50px ml-120px">
-				<Button label="申請者登入" class="h-75px w-400px" />
+				<router-link to="/login">
+					<Button :label="$t('申請者登入')" class="h-75px w-400px" />
+				</router-link>
 			</div>
 
 			<div class="mt-50px ml-120px">
-				<Button label="審查委員/行政人員登入" class="h-75px w-400px" />
+				<router-link to="/login">
+					<Button
+						:label="$t('審查委員/行政人員登入')"
+						class="h-75px w-400px"
+					/>
+				</router-link>
 			</div>
 		</div>
 	</div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,20 @@
 import { defineConfig } from "vite";
+import { resolve } from "node:path";
 import WindiCSS from "vite-plugin-windicss";
 import vue from "@vitejs/plugin-vue";
 import Icons from "unplugin-icons/vite";
 import IconsResolver from "unplugin-icons/resolver";
 import ViteComponents from "unplugin-vue-components/vite";
+import vueI18n from "@intlify/vite-plugin-vue-i18n";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		vue({
 			include: [/\.vue$/],
+		}),
+		vueI18n({
+			include: resolve(__dirname, "./locales/**"),
 		}),
 		WindiCSS(),
 		ViteComponents({
