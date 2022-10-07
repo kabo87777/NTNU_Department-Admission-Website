69b82b6c70e3e4bed8665d2808cd0fdd4819d85a
KutsunaSubaRya <index20010928@gmail.com>
2022-10-08 03:07:47 +0800

    chore: delete primeflex package

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ import "./styles/style.css";
 // Import Style
 import "primevue/resources/themes/lara-light-blue/theme.css";
 import "primevue/resources/primevue.min.css";
-import "primeflex/primeflex.css";
 import "primeicons/primeicons.css";
 
 // Import via Module
