ab025493c426a2104b529688adb983e324553cb9
Birkhoff Lee <git@birkhoff.me>
2022-10-09 00:39:01 +0800

    fix(i18n): enable globalInjection

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,6 +2,7 @@ import { createI18n } from "vue-i18n";
 import messages from "@intlify/vite-plugin-vue-i18n/messages";
 
 const i18n = createI18n({
+	globalInjection: true,
 	locale:
 		window.localStorage.getItem("lastLocale") || navigator.language || "",
 	fallbackLocale: "zh",
