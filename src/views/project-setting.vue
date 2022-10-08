43e195b7342c36e8c002ec91a21b34450a00a278
kabo chang <brian891125@gmail.com>
2022-10-08 21:51:18 +0800

    fix:fix pnpm lint

--- src/views/project-setting.vue
+++ src/views/project-setting.vue
@@ -1,139 +1,146 @@
 <template>
 	<div class="ml-128px">
 		<h1 class="text-4xl text-bold tracking-widest">專案設定</h1>
-	<Divider class="bg-ntnuRed" />
-	<h2 class="text-2xl text-bold tracking-widest inline-block">
-		111年研究生審查
-	</h2>
-	<Button class="w-100px h-40px !ml-20px p-button-outlined p-button-success">
-		<img
-			alt="logo"
-			src="../../src/assets/project-setting/Check_fill.png"
-			style="width: 1.5rem"
-			class="fill-green-500"
-		/>
-		<span class="tracking-1px"> 保存 </span>
-	</Button>
-	<Button class="w-100px h-40px !ml-20px p-button-outlined p-button-danger">
-		<img
-			alt="logo"
-			src="../../src/assets/project-setting/Close_round.png"
-			style="width: 1.5rem"
-			class="fill-green-500"
-		/>
-		<span class="tracking-1px"> 刪除 </span>
-	</Button>
-	<h3 class="text-xs tracking-widest text-gray-500">
-		2022/09/15 · 新增到專案
-	</h3>
-	<h5 class="text-base tracking-widest mt-30px">專案名稱 (修改) :</h5>
-	<InputText
-		type="text"
-		v-model="profect_name"
-		class="w-960px h-44px mt-10px"
-	/>
-	<div>
-		<h5 class="text-base tracking-widest mt-30px">屬性 :</h5>
-		<Dropdown
-			v-model="selected_type"
-			:options="types"
-			optionLabel="type_name"
-			optionValue="type_name"
-			class="!w-320px !h-44px mt-10px"
-		/>
-	</div>
-	<div class="inline-block">
-		<h5 class="text-base tracking-widest mt-30px">申請端開放時間/日期 :</h5>
-		<Calendar
-			inputId="icon"
-			v-model="application_start_time"
-			:showIcon="true"
-			:showTime="true"
-			class="w-320px h-44px mt-10px"
-		/>
-	</div>
-	<div class="inline-block ml-100px">
-		<h5 class="text-base tracking-widest mt-30px">申請端關閉時間/日期 :</h5>
-		<Calendar
-			inputId="icon"
-			v-model="application_end_time"
-			:showIcon="true"
-			:showTime="true"
-			class="w-320px h-44px mt-10px"
-		/>
-	</div>
-	<br />
-	<div class="inline-block">
-		<h5 class="text-base tracking-widest mt-30px">
-			審查端第一階段開放時間/日期 :
-		</h5>
-		<Calendar
-			inputId="icon"
-			v-model="review_stage1_start_time"
-			:showIcon="true"
-			:showTime="true"
-			class="w-320px h-44px mt-10px"
-		/>
-	</div>
-	<div class="inline-block ml-100px">
-		<h5 class="text-base tracking-widest mt-30px">
-			審查端第一階段關閉時間/日期 :
-		</h5>
-		<Calendar
-			inputId="icon"
-			v-model="review_stage1_end_time"
-			:showIcon="true"
-			:showTime="true"
-			class="w-320px h-44px mt-10px"
-		/>
-	</div>
-	<br />
-	<div class="inline-block">
-		<h5 class="text-base tracking-widest mt-30px">
-			審查端第二階段開放時間/日期 :
-		</h5>
-		<Calendar
-			inputId="icon"
-			v-model="review_stage2_start_time"
-			:showIcon="true"
-			:showTime="true"
-			class="w-320px h-44px mt-10px"
-		/>
-	</div>
-	<div class="inline-block ml-100px">
-		<h5 class="text-base tracking-widest mt-30px">
-			審查端第二階段關閉時間/日期 :
-		</h5>
-		<Calendar
-			inputId="icon"
-			v-model="review_stage2_end_time"
-			:showIcon="true"
-			:showTime="true"
-			class="w-320px h-44px mt-10px"
-		/>
-	</div>
-	<br />
-	<div>
-		<Checkbox
-			inputId="binary"
-			v-model="checked"
-			:binary="true"
-			class="!align-top mt-30px"
-		/>
-		<h5
-			class="text-base tracking-widest mt-30px ml-10px align-top inline-block"
+		<Divider class="bg-ntnuRed" />
+		<h2 class="text-2xl text-bold tracking-widest inline-block">
+			111年研究生審查
+		</h2>
+		<Button
+			class="w-100px h-40px !ml-20px p-button-outlined p-button-success"
 		>
-			專案詳細 :
-		</h5>
-		<Textarea
-			v-model="project_details"
-			rows="5"
-			cols="30"
-			class="w-950px h-320px !mt-30px !ml-10px"
+			<img
+				alt="logo"
+				src="../../src/assets/project-setting/Check_fill.png"
+				style="width: 1.5rem"
+				class="fill-green-500"
+			/>
+			<span class="tracking-1px"> 保存 </span>
+		</Button>
+		<Button
+			class="w-100px h-40px !ml-20px p-button-outlined p-button-danger"
+		>
+			<img
+				alt="logo"
+				src="../../src/assets/project-setting/Close_round.png"
+				style="width: 1.5rem"
+				class="fill-green-500"
+			/>
+			<span class="tracking-1px"> 刪除 </span>
+		</Button>
+		<h3 class="text-xs tracking-widest text-gray-500">
+			2022/09/15 · 新增到專案
+		</h3>
+		<h5 class="text-base tracking-widest mt-30px">專案名稱 (修改) :</h5>
+		<InputText
+			type="text"
+			v-model="profect_name"
+			class="w-960px h-44px mt-10px"
 		/>
+		<div>
+			<h5 class="text-base tracking-widest mt-30px">屬性 :</h5>
+			<Dropdown
+				v-model="selected_type"
+				:options="types"
+				optionLabel="type_name"
+				optionValue="type_name"
+				class="!w-320px !h-44px mt-10px"
+			/>
+		</div>
+		<div class="inline-block">
+			<h5 class="text-base tracking-widest mt-30px">
+				申請端開放時間/日期 :
+			</h5>
+			<Calendar
+				inputId="icon"
+				v-model="application_start_time"
+				:showIcon="true"
+				:showTime="true"
+				class="w-320px h-44px mt-10px"
+			/>
+		</div>
+		<div class="inline-block ml-100px">
+			<h5 class="text-base tracking-widest mt-30px">
+				申請端關閉時間/日期 :
+			</h5>
+			<Calendar
+				inputId="icon"
+				v-model="application_end_time"
+				:showIcon="true"
+				:showTime="true"
+				class="w-320px h-44px mt-10px"
+			/>
+		</div>
+		<br />
+		<div class="inline-block">
+			<h5 class="text-base tracking-widest mt-30px">
+				審查端第一階段開放時間/日期 :
+			</h5>
+			<Calendar
+				inputId="icon"
+				v-model="review_stage1_start_time"
+				:showIcon="true"
+				:showTime="true"
+				class="w-320px h-44px mt-10px"
+			/>
+		</div>
+		<div class="inline-block ml-100px">
+			<h5 class="text-base tracking-widest mt-30px">
+				審查端第一階段關閉時間/日期 :
+			</h5>
+			<Calendar
+				inputId="icon"
+				v-model="review_stage1_end_time"
+				:showIcon="true"
+				:showTime="true"
+				class="w-320px h-44px mt-10px"
+			/>
+		</div>
+		<br />
+		<div class="inline-block">
+			<h5 class="text-base tracking-widest mt-30px">
+				審查端第二階段開放時間/日期 :
+			</h5>
+			<Calendar
+				inputId="icon"
+				v-model="review_stage2_start_time"
+				:showIcon="true"
+				:showTime="true"
+				class="w-320px h-44px mt-10px"
+			/>
+		</div>
+		<div class="inline-block ml-100px">
+			<h5 class="text-base tracking-widest mt-30px">
+				審查端第二階段關閉時間/日期 :
+			</h5>
+			<Calendar
+				inputId="icon"
+				v-model="review_stage2_end_time"
+				:showIcon="true"
+				:showTime="true"
+				class="w-320px h-44px mt-10px"
+			/>
+		</div>
+		<br />
+		<div>
+			<Checkbox
+				inputId="binary"
+				v-model="checked"
+				:binary="true"
+				class="!align-top mt-30px"
+			/>
+			<h5
+				class="text-base tracking-widest mt-30px ml-10px align-top inline-block"
+			>
+				專案詳細 :
+			</h5>
+			<Textarea
+				v-model="project_details"
+				rows="5"
+				cols="30"
+				class="w-950px h-320px !mt-30px !ml-10px"
+			/>
+		</div>
 	</div>
-	</div>
-	
 </template>
 
 <script setup lang="ts">
