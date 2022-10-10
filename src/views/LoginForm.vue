51d697df26014b276895c7f940a98e9b3e2728d9
Howard Guo <50100922+toto6038@users.noreply.github.com>
2022-10-11 02:18:59 +0800

    refactor: split out login form as a component

---
+++ src/views/LoginForm.vue
@@ -0,0 +1,46 @@
+<template>
+
+<div class="mx-30">
+
+  <h2 class="text-3xl font-bold text-gray-500 block">{{ title }}</h2>
+  
+  <form action="">
+    
+    <div class="mt-5">
+      <label for="" class="block">{{$t("准考證號碼")}}</label>
+      <div class="pt-1">
+        <InputText class="w-full" type="text" />
+      </div>
+    </div>
+    
+    <div class="mt-3">
+      <label for="" class="block">{{$t("密碼")}}</label>
+
+      <div class="pt-1">
+        <InputText class="w-full" type="password"></InputText>
+      </div>
+    </div>
+  
+    <div class="mt-4">
+      <Checkbox />
+      <label for="" class="ml-1">{{$t("記住我")}}</label>
+      <a href="" class="float-right text-blue-500">{{$t("忘記密碼")}}</a>
+    </div>
+  
+    <div class="mt-5">
+      <Button class="w-full" :label="$t('登入')" />
+    </div>
+  </form>
+</div>
+
+</template>
+
+<script setup lang="ts">
+import InputText from "primevue/inputtext";
+import Checkbox from "primevue/checkbox";
+import Button from "primevue/button";
+
+defineProps<{
+  title?: string
+}>()
+</script>
