2caf21b899a5c590f9b252f29bf497fc8400b742
Howard Guo <50100922+toto6038@users.noreply.github.com>
2022-10-11 02:16:08 +0800

    refactor: split out landing view layout as a component

---
+++ src/views/LandingViewLayout.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="grid grid-cols-9 gap-x-4">
+		<div class="col-span-5">
+			<img
+				src="/assets/login-page/Login-img.png"
+				alt=""
+				class="overflow-auto"
+			/>
+		</div>
+
+		<div class="col-span-4 mt-20">
+			<!-- Logo -->
+			<div class="flex mt-2 justify-center items-center">
+				<img
+					src="/assets/login-page/NTNU-logo-B1.png"
+					alt=""
+					class="h-12"
+				/>
+				<Divider layout="vertical" />
+				<div>
+					<h1 class="text-3xl font-bold text-gray-500">
+						資訊工程學系
+					</h1>
+					<h3 class="text-xs text-gray-400">
+						Department of Computer Science and Information
+						Enginering
+					</h3>
+				</div>
+			</div>
+
+			<div v-if="hasSlot('form')" class="mt-20">
+				<!-- Form -->
+				<slot name="form"></slot>
+			</div>
+
+		</div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+import { useSlots } from 'vue';
+
+const vue_slots=useSlots()
+
+const hasSlot = (name: string)=>{
+	return !!vue_slots[name]
+}
+</script>
