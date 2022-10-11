dcc21dd729dd1815b74cafd16a1bbccc4a72c2cf
PTLee70 <40847057s@gapps.ntnu.edu.tw>
2022-10-11 12:00:55 +0800

    feat(link to applicantscup upload lis): 1. link to application upload list in sidebar 2. fix select button width in project setting page 3. add translation to sidebar dropdown menu

--- src/views/admission/manager/applicantsUploadList/applicantsUploadList.vue
+++ src/views/admission/manager/applicantsUploadList/applicantsUploadList.vue
@@ -1,3 +1,10 @@
-<template>This is applicants upload list page</template>
+<template>
+    <div class="ml-128px">
+        <div class="text-32px font-medium">上傳資料列表</div>
+        <div class="bigRedDivider"></div>
+    </div>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import "../../../../styles/customize.css";
+</script>
