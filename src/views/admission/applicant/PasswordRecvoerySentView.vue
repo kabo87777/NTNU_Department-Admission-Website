b85890f953dfd1cd9cfbfac3da50c076d8bd5c1c
Howard Guo <50100922+toto6038@users.noreply.github.com>
2022-10-11 19:59:30 +0800

    feat: add password recovery email sent page

---
+++ src/views/admission/applicant/PasswordRecvoerySentView.vue
@@ -0,0 +1,16 @@
+<template>
+
+<BaseLayout>
+  <template #form>
+    <PwdRecovery role="applicant"></PwdRecovery>
+  </template>
+</BaseLayout>
+
+</template>
+
+<script setup lang="ts">
+import BaseLayout from '@/components/LandingViewLayout.vue'
+import PwdRecovery from '@/components/PasswordRecoveryEmailSent.vue'
+import { ref } from 'vue';
+
+</script>
