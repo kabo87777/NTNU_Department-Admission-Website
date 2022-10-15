b68679ef59b663e453d0cd7716d9207afffbde04
KutsunaSubaRya <index20010928@gmail.com>
2022-10-15 22:38:24 +0800

    feat(mainpage): add mainpage to choose admission or recruitment

---
+++ src/views/MainPage.vue
@@ -0,0 +1,67 @@
+<template>
+	<div class="grid grid-cols-2 gap-15px">
+		<div>
+			<img src="/assets/login-page/Login-img.png" class="h-screen" />
+		</div>
+		<div class="m-auto">
+			<div class="flex m-auto h-15">
+				<div>
+					<img
+						src="/assets/login-page/NTNU-logo-B1.png"
+						class="h-15"
+					/>
+				</div>
+				<Divider layout="vertical" />
+				<div class="mt-1.5">
+					<div class="text-4xl font-bold text-gray-500">
+						資訊工程學系
+					</div>
+					<div class="text-xs text-gray-400">
+						Department of Computer Science and Information
+						Enginering
+					</div>
+				</div>
+			</div>
+			<div>
+				<div class="mt-100px ml-204px text-4xl font-bold text-gray-500">
+					{{ $t("資訊工程學系報名系統") }}
+				</div>
+				<div class="ml-204px text-xs text-gray-500">
+					NTNU CSIE Admissions
+				</div>
+			</div>
+			<div class="mt-50px ml-184px userLogin">
+				<router-link to="/admission">
+					<Button class="bg-yellow h-95px w-360px">
+						<div class="m-auto text-2xl">
+							<div>{{ $t("招生系統") }}</div>
+						</div>
+					</Button>
+				</router-link>
+			</div>
+			<div class="mt-50px ml-184px">
+				<router-link to="/recruitment">
+					<Button class="bg-red h-95px w-360px">
+						<div class="m-auto text-2xl">
+							<div>{{ $t("教師聘請系統") }}</div>
+						</div>
+					</Button>
+				</router-link>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import Button from "primevue/button";
+import Divider from "primevue/divider";
+</script>
+
+<style setup lang="css">
+.bg-yellow {
+	background-color: #b8b800;
+}
+.bg-red {
+	background-color: #ff0000;
+}
+</style>
