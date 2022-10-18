8d30583a979e543946d2619e957d758358741cab
40947010S <iamtim303@gmail.com>
2022-10-19 05:12:38 +0800

    feat(basicInfo additionallInfo): finish

--- src/views/admission/manager/applicantsUploadList/reviewTabs/additionalInfo.vue
+++ src/views/admission/manager/applicantsUploadList/reviewTabs/additionalInfo.vue
@@ -2,7 +2,9 @@
 	<div class="mt-16px">
 		<div class="mt-16px">
 			<div>
-				<label class="ml-8px text-24px font-medium">系統設置</label>
+				<label class="ml-8px text-24px font-medium">{{
+					$t("系統設置")
+				}}</label>
 			</div>
 		</div>
 		<div>
@@ -16,8 +18,8 @@
 			</SelectButton>
 		</div>
 		<div class="flex mt-16px">
-			<div class="w-1/3 pl-16px">開放時間</div>
-			<div class="w-1/3 pl-16px">截止時間</div>
+			<div class="w-1/3 pl-16px">{{ $t("開放時間") }}</div>
+			<div class="w-1/3 pl-16px">{{ $t("截止時間") }}</div>
 		</div>
 		<div class="flex mt-16px">
 			<div p-fluid grid formgrid>
@@ -48,11 +50,13 @@
 
 		<div class="mt-16px">
 			<div>
-				<label class="ml-8px text-24px font-medium">補件需求</label>
+				<label class="ml-8px text-24px font-medium">{{
+					$t("補件需求")
+				}}</label>
 			</div>
 		</div>
 		<div class="block mt-16px">
-			<div class="w-1/3 pl-16px">補件需求說明:</div>
+			<div class="w-1/3 pl-16px">{{ $t("補件需求說明") }}:</div>
 			<div class="mt-8px ml-16px">
 				<InputText type="text" v-model="inputvalue" class="h-10" />
 				<span :style="{ marginLeft: '.5em' }">{{ inputvalue }}</span>
@@ -60,13 +64,15 @@
 		</div>
 		<div class="mt-16px ml-16px">
 			<Checkbox inputId="text" v-model="textChecked" :binary="true" />
-			<label for="text" class="ml-8px font-medium">文字輸入框</label>
+			<label for="text" class="ml-8px font-medium">{{
+				$t("文字輸入框")
+			}}</label>
 		</div>
 		<div class="flex mt-16px">
 			<div class="mt-16px ml-16px">
 				<Checkbox inputId="file" v-model="fileChecked" :binary="true" />
 				<label for="file" class="ml-8px font-medium"
-					>上傳檔案，檔案類型:</label
+					>{{ $t("上傳檔案，檔案類型") }}:</label
 				>
 			</div>
 			<div class="ml-16px">
@@ -86,23 +92,24 @@
 				<div class="smallRedDivider absolute right-0"></div>
 			</div>
 		</div>
-		
+
 		<ParagraphDivider />
 
 		<div class="mt-16px">
 			<div>
-				<label class="ml-8px text-24px font-medium">提交內容</label>
+				<label class="ml-8px text-24px font-medium">{{
+					$t("提交內容")
+				}}</label>
 			</div>
 		</div>
-		<div class="mt-16px">※ 尚未提交任何文字，檔案</div>
+		<div class="mt-16px">{{ $t("尚未提交任何文字，檔案") }}</div>
 		<div class="bigRedDivider"></div>
 		<div class="mt-16px ml-20px grid justify-center">
 			<Button
 				@click="displayEmail"
-				label="Secondary"
 				class="grid justify-center p-button-outlined p-button-secondary text-20px tracking-wide"
 			>
-				寄送E-mail通知
+				{{ $t("寄送E-mail通知") }}
 			</Button>
 		</div>
 
@@ -115,11 +122,11 @@
 	>
 		<divider class="!mt-0px" />
 		<p class="mt-24px text-base tracking-2px">
-			確定要寄送email通知已開啟補件系統嗎?
+			{{ $t("確定要寄送email通知已開啟補件系統嗎") }}?
 		</p>
 		<div class="flex">
 			<Button
-				@click="closeEmail "
+				@click="closeEmail"
 				class="p-button-outlined p-button-success !ml-70px !mt-26px !w-105px !h-44px"
 			>
 				<img
@@ -129,7 +136,7 @@
 					class="!ml-0px"
 				/>
 				<span class="text-left text-xl">
-					確定
+					{{ $t("確定") }}
 				</span>
 			</Button>
 			<Button
@@ -180,8 +187,8 @@ let nextYear = nextMonth === 0 ? year + 1 : year;
 
 const inputvalue = ref();
 const translation = {
-	basicCol: t("啟用"),
-	attachmentCol: t("關閉"),
+	basicCol: t("Turn ON"),
+	attachmentCol: t("TurnOFF"),
 };
 const activeTab = ref({ name: translation.basicCol, value: 1 });
 const tabOptions = ref([
--- src/views/admission/manager/applicantsUploadList/reviewTabs/basicInfo.vue
+++ src/views/admission/manager/applicantsUploadList/reviewTabs/basicInfo.vue
@@ -1,18 +1,19 @@
 <template>
 	<div class="mt-16px pl-8px pr-8px">
-		<div class="text-24px font-medium">姓名資訊</div>
+		<div class="text-24px font-medium">{{ $t("姓名資訊") }}</div>
 		<div class="mt-24px flex">
 			<div class="w-1/3">
-				<div class="text-16px">稱謂：</div>
-				<InputText
-					type="text"
-					v-model="title"
-					placeholder="Text"
-					class="!border-gray-900 !w-300px !h-40px !mt-5px "
+				<div class="text-16px">{{ $t("稱謂") }}：</div>
+				<Dropdown
+					v-model="selected_title"
+					:options="titles"
+					optionLabel="type_name"
+					optionValue="type_name"
+					class="!border-gray-900 !w-300px !h-40px mt-10px"
 				/>
 			</div>
 			<div class="w-1/3">
-				<div class="text-16px">後綴：</div>
+				<div class="text-16px">{{ $t("後綴") }}：</div>
 				<InputText
 					type="text"
 					v-model="suffix"
@@ -23,7 +24,7 @@
 		</div>
 		<div class="mt-24px flex">
 			<div class="w-1/2">
-				<div class="text-16px">中文姓氏：</div>
+				<div class="text-16px">{{ $t("中文姓氏") }}：</div>
 				<InputText
 					type="text"
 					v-model="title"
@@ -32,7 +33,7 @@
 				/>
 			</div>
 			<div class="w-1/2">
-				<div class="text-16px">中文名字：</div>
+				<div class="text-16px">{{ $t("中文名字") }}：</div>
 				<InputText
 					type="text"
 					v-model="suffix"
@@ -43,16 +44,25 @@
 		</div>
 		<div class="mt-24px flex">
 			<div class="w-1/3">
-				<div class="text-16px">英文姓氏：</div>
+				<div class="text-16px">{{ $t("英文姓氏") }}：</div>
 				<InputText
 					type="text"
 					v-model="title"
 					placeholder="Text"
-					class="!border-gray-900 !w-300px !h-40px !mt-5px "
+					class="!border-gray-900 !w-300px !h-40px !mt-5px"
 				/>
 			</div>
 			<div class="w-1/3">
-				<div class="text-16px">英文名字：</div>
+				<div class="text-16px">{{ $t("英文中間名字") }}：</div>
+				<InputText
+					type="text"
+					v-model="suffix"
+					placeholder="Text"
+					class="!border-gray-900 !w-300px !h-40px !mt-5px"
+				/>
+			</div>
+			<div class="w-1/3">
+				<div class="text-16px">{{ $t("英文名字") }}：</div>
 				<InputText
 					type="text"
 					v-model="suffix"
@@ -62,31 +72,33 @@
 			</div>
 		</div>
 		<ParagraphDivider />
-		<div class="mt-24px text-24px font-medium">入職身分(本地人士)</div>
-		<div class="mt-24px flex">
-			<div class="w-1/3">
-				<div class="text-16px">入學身分：</div>
-				<InputText
-					type="text"
-					v-model="title"
-					placeholder="Text"
-					class="!border-gray-900 !w-300px !h-40px !mt-5px "
-				/>
-			</div>
-			
+		<div class="mt-24px text-24px font-medium">
+			{{ $t("入學身分(本地人士)") }}
 		</div>
 		<div class="mt-24px flex">
 			<div class="w-1/3">
-				<div class="text-16px">國籍：</div>
+				<div class="text-16px">{{ $t("入學身分") }}：</div>
+				<Dropdown
+					v-model="selected_type"
+					:options="types"
+					optionLabel="type_name"
+					optionValue="type_name"
+					class="!border-gray-900 !w-300px !h-40px mt-10px"
+				/>
+			</div>
+		</div>
+		<div class="mt-24px flex">
+			<div class="w-1/3">
+				<div class="text-16px">{{ $t("國籍") }}：</div>
 				<InputText
 					type="text"
 					v-model="title"
 					placeholder="Text"
-					class="!border-gray-900 !w-300px !h-40px !mt-5px "
+					class="!border-gray-900 !w-300px !h-40px !mt-5px"
 				/>
 			</div>
 			<div class="w-1/3">
-				<div class="text-16px">身份證字號：</div>
+				<div class="text-16px">{{ $t("身份證字號") }}：</div>
 				<InputText
 					type="text"
 					v-model="suffix"
@@ -96,24 +108,30 @@
 			</div>
 		</div>
 		<ParagraphDivider />
-		<div class="mt-24px text-24px font-medium">現居地址</div>
+		<div class="mt-24px text-24px font-medium">{{ $t("現居地址") }}</div>
 		<div class="mt-16px ml-16px">
-			<Checkbox inputId="text" v-model="textCheckedAddress" :binary="true" />
-			<label for="text" class="ml-8px font-medium">與戶籍地址資訊相同</label>
+			<Checkbox
+				inputId="text"
+				v-model="textCheckedAddress"
+				:binary="true"
+			/>
+			<label for="text" class="ml-8px font-medium">{{
+				$t("與戶籍地址資訊相同")
+			}}</label>
 		</div>
-		
+
 		<div class="mt-24px flex">
 			<div class="w-1/2">
-				<div class="text-16px">國家：</div>
+				<div class="text-16px">{{ $t("國家") }}：</div>
 				<InputText
 					type="text"
-					v-model="title"
+					v-model="suffix"
 					placeholder="Text"
 					class="!border-gray-900 !w-440px !h-40px !mt-5px"
 				/>
 			</div>
 			<div class="w-1/2">
-				<div class="text-16px">州：</div>
+				<div class="text-16px">{{ $t("州") }}：</div>
 				<InputText
 					type="text"
 					v-model="suffix"
@@ -124,7 +142,7 @@
 		</div>
 		<div class="mt-24px flex">
 			<div class="w-1/2">
-				<div class="text-16px">城市：</div>
+				<div class="text-16px">{{ $t("城市") }}：</div>
 				<InputText
 					type="text"
 					v-model="title"
@@ -133,7 +151,7 @@
 				/>
 			</div>
 			<div class="w-1/2">
-				<div class="text-16px">郵遞區號：</div>
+				<div class="text-16px">{{ $t("郵遞區號") }}：</div>
 				<InputText
 					type="text"
 					v-model="suffix"
@@ -142,9 +160,9 @@
 				/>
 			</div>
 		</div>
-		<div class="mt-24px ">
+		<div class="mt-24px">
 			<div class="w-2/3">
-				<div class="text-16px">街道地址：</div>
+				<div class="text-16px">{{ $t("街道地址") }}：</div>
 				<InputText
 					type="text"
 					v-model="suffix"
@@ -152,80 +170,115 @@
 					class="!border-gray-900 !w-440px !h-40px !mt-5px"
 				/>
 			</div>
-			
 		</div>
 		<ParagraphDivider />
-		<div class="mt-24px text-24px font-medium">身份資料</div>
+		<div class="mt-24px text-24px font-medium">{{ $t("身份資料") }}</div>
 		<div class="mt-24px flex">
 			<div class="w-1/3">
-				<div class="text-16px">法定性別：</div>
+				<div class="text-16px">{{ $t("法定性別") }}：</div>
 			</div>
 		</div>
 
 		<div class="mt-24px flex">
 			<div class="w-1/5">
-				<Checkbox inputId="text" v-model="textCheckedMan" :binary="true" />
-				<label for="text" class="ml-8px font-medium">男性</label>
+				<Checkbox
+					inputId="text"
+					v-model="textCheckedMan"
+					:binary="true"
+				/>
+				<label for="text" class="ml-8px font-medium">{{
+					$t("男性")
+				}}</label>
 			</div>
 			<div class="w-1/5">
-				<Checkbox inputId="text" v-model="textCheckedWoman" :binary="true" />
-				<label for="text" class="ml-8px font-medium">女性</label>
+				<Checkbox
+					inputId="text"
+					v-model="textCheckedWoman"
+					:binary="true"
+				/>
+				<label for="text" class="ml-8px font-medium">{{
+					$t("女性")
+				}}</label>
 			</div>
 		</div>
 		<div class="mt-24px flex">
 			<div class="w-1/3">
-				<div class="text-16px">性別認同：</div>
+				<div class="text-16px">{{ $t("性別認同") }}：</div>
 			</div>
 		</div>
 		<div class="mt-24px flex">
 			<div class="w-1/5">
-				<Checkbox inputId="text" v-model="textCheckedM" :binary="true" />
-				<label for="text" class="ml-8px font-medium">男性</label>
+				<Checkbox
+					inputId="text"
+					v-model="textCheckedM"
+					:binary="true"
+				/>
+				<label for="text" class="ml-8px font-medium">{{
+					$t("男性")
+				}}</label>
 			</div>
 			<div class="w-1/5">
-				<Checkbox inputId="text" v-model="textCheckedW" :binary="true" />
-				<label for="text" class="ml-8px font-medium">女性</label>
+				<Checkbox
+					inputId="text"
+					v-model="textCheckedW"
+					:binary="true"
+				/>
+				<label for="text" class="ml-8px font-medium">{{
+					$t("女性")
+				}}</label>
 			</div>
 			<div class="w-1/5">
-				<Checkbox inputId="text" v-model="textCheckedB" :binary="true" />
-				<label for="text" class="ml-8px font-medium">非二元性別</label>
+				<Checkbox
+					inputId="text"
+					v-model="textCheckedB"
+					:binary="true"
+				/>
+				<label for="text" class="ml-8px font-medium">{{
+					$t("非二元性別")
+				}}</label>
 			</div>
 		</div>
 		<div class="mt-24px flex">
 			<div class="w-1/3">
-				<div class="text-16px">出生國家：</div>
-				<InputText
-					type="text"
-					v-model="title"
-					placeholder="Text"
-					class="!border-gray-900 !w-300px !h-40px !mt-5px "
+				<div class="text-16px">{{ $t("出生國家") }}：</div>
+				<Dropdown
+					v-model="country"
+					:options="cities"
+					optionLabel="name"
+					class="!border-gray-900 !w-300px !h-40px mt-10px"
 				/>
 			</div>
 			<div class="w-1/3">
-				<div class="text-16px">出生日期：</div>
-				<InputText
-					type="text"
-					v-model="suffix"
-					placeholder="Text"
+				<div class="text-16px">{{ $t("出生日期") }}：</div>
+				<Calendar
 					class="!border-gray-900 !w-300px !h-40px !mt-5px"
-				/>
+					inputId="time24"
+					v-model="date1"
+					:showTime="true"
+					:showSeconds="true"
+					:showIcon="true"
+				>
+				</Calendar>
 			</div>
 		</div>
 		<div class="flex mt-16px">
 			<div class="mt-16px ml-16px">
-				<Checkbox inputId="file" v-model="nationalSecond" :binary="true" />
+				<Checkbox
+					inputId="file"
+					v-model="nationalSecond"
+					:binary="true"
+				/>
 				<label for="file" class="ml-8px font-medium"
-					>是否具有雙重國籍，第二國籍身份:</label
+					>{{ $t("是否有雙重國籍，第二國籍身分") }}:</label
 				>
 			</div>
 			<div class="ml-16px">
 				<span class="p-float-label">
 					<Dropdown
-						id="dropdown"
-						v-model="value8"
+						v-model="countrySecond"
 						:options="cities"
 						optionLabel="name"
-						class="h-10 w-40"
+						class="!border-gray-900 !w-300px !h-40px mt-10px"
 					/>
 				</span>
 			</div>
@@ -234,7 +287,7 @@
 			<div class="mt-16px ml-16px">
 				<Checkbox inputId="file" v-model="national" :binary="true" />
 				<label for="file" class="ml-8px font-medium"
-					>非台灣國籍，簽證類型:</label
+					>{{ $t("非台灣國籍，簽證類型") }}:</label
 				>
 			</div>
 			<div class="ml-16px">
@@ -250,32 +303,30 @@
 			</div>
 		</div>
 		<ParagraphDivider />
-		<div class="mt-24px text-24px font-medium">聯絡方式</div>
+		<div class="mt-24px text-24px font-medium">{{ $t("聯絡方式") }}</div>
 		<div class="mt-24px flex">
 			<div class="w-1/2">
-				<div class="text-16px">主要聯絡電話：</div>
+				<div class="text-16px">{{ $t("主要聯絡電話") }}：</div>
 				<InputText
 					type="text"
 					v-model="title"
 					placeholder="Text"
-					class="!border-gray-900 !w-300px !h-40px !mt-5px "
+					class="!border-gray-900 !w-300px !h-40px !mt-5px"
 				/>
 			</div>
 			<div class="w-1/2">
-				<div class="text-16px">次要聯絡電話：</div>
+				<div class="text-16px">{{ $t("次要聯絡電話") }}：</div>
 				<InputText
 					type="text"
 					v-model="title"
 					placeholder="Text"
-					class="!border-gray-900 !w-300px !h-40px !mt-5px "
+					class="!border-gray-900 !w-300px !h-40px !mt-5px"
 				/>
 			</div>
-			
 		</div>
 		<div class="mt-24px flex">
-			
 			<div class="w-1/2">
-				<div class="text-16px">行動電話：</div>
+				<div class="text-16px">{{ $t("行動電話") }}：</div>
 				<InputText
 					type="text"
 					v-model="suffix"
@@ -286,14 +337,13 @@
 			<div class="mt-16px ml-16px">
 				<Checkbox inputId="file" v-model="phone" :binary="true" />
 				<label for="file" class="ml-8px font-medium"
-					>行動電話與主要聯絡電話相同:</label
+					>{{ $t("行動電話與主要聯絡電話相同") }}:</label
 				>
 			</div>
 		</div>
 		<div class="mt-24px flex">
-			
 			<div class="w-1/3">
-				<div class="text-16px">電子信箱：</div>
+				<div class="text-16px">{{ $t("電子信箱") }}：</div>
 				<InputText
 					type="text"
 					v-model="suffix"
@@ -301,19 +351,15 @@
 					class="!border-gray-900 !w-300px !h-40px !mt-5px"
 				/>
 			</div>
-			
 		</div>
 		<div class="mt-24px flex">
 			<div class="mt-16px ml-16px">
 				<Checkbox inputId="file" v-model="message" :binary="true" />
 				<label for="file" class="ml-8px font-medium"
-					>願意承擔手機簡訊費用:</label
+					>{{ $t("願意承擔手機簡訊費用") }}:</label
 				>
 			</div>
 		</div>
-		
-		
-
 	</div>
 </template>
 
@@ -327,15 +373,15 @@ import MultiSelect from "primevue/multiselect";
 import InputText from "primevue/inputtext";
 import Checkbox from "primevue/checkbox";
 import Button from "primevue/button";
-import Dropdown from "primevue/dropdown";
 import Calendar from "primevue/calendar";
-import Dialog from "primevue/dialog";
+import Dropdown from "primevue/dropdown";
 import ParagraphDivider from "../../../../../styles/paragraphDivider.vue";
 import { ref, watch } from "vue";
 import { useI18n } from "vue-i18n";
 const props = defineProps(["userId"]);
 const title = ref("");
 const suffix = ref("");
+const selected_country = ref();
 const textCheckedAddress = ref(false);
 const textCheckedMan = ref(false);
 const textCheckedWoman = ref(false);
@@ -344,9 +390,23 @@ const textCheckedW = ref(false);
 const textCheckedB = ref(false);
 const phone = ref(false);
 const message = ref(false);
-const nationalSecond = ref(false);
-const national = ref(false);
-
-
+const countrySecond = ref();
+const country = ref();
+const date1 = ref();
+const selected_type = ref({});
+const types = ref([
+	{ type_name: "特殊選才" },
+	{ type_name: "碩士班徵試入學" },
+	{ type_name: "博士班徵試入學" },
+]);
+const selected_title = ref({});
+const titles = ref([{ type_name: "先生" }, { type_name: "小姐" }]);
+const cities = ref([
+	{ name: "New York", code: "NY" },
+	{ name: "Rome", code: "RM" },
+	{ name: "London", code: "LDN" },
+	{ name: "Istanbul", code: "IST" },
+	{ name: "Paris", code: "PRS" },
+]);
 console.log(props.userId, "basic info tab");
 </script>
