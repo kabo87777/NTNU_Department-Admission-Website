bb08cc5fee79f79e15f85bed0cc17c0bce8ec6d2
PTLee70 <40847057s@gapps.ntnu.edu.tw>
2022-10-18 11:29:53 +0000

    fix typescript error issue

--- src/views/admission/manager/applicantsUploadList/reviewTabs/attachmentInfo.vue
+++ src/views/admission/manager/applicantsUploadList/reviewTabs/attachmentInfo.vue
@@ -6,36 +6,32 @@
 
 		<!-- SCHOOL EXPERIENCE -->
 		<div v-for="(item, index) in schoolExpList" :key="index">
-			<div v-if="item.state === 1">
-				<ReviewState
-					category="就學經歷"
-					:itemName="item.name"
-					:order="index + 1"
-					@edit="reviewToEdit"
-				/>
-			</div>
-			<div v-else-if="item.state === 2">
-				<EditState
-					category="就學經歷"
-					:itemName="item.name"
-					:order="index + 1"
-					@cancel="editToReview"
-				/>
-			</div>
-			<div v-else-if="item.state === 3">
-				<CreateState
-					category="就學經歷"
-					:order="index + 1"
-					@edit="createToEdit"
-				/>
-			</div>
-			<div v-else-if="item.state === 4">
-				<EditState
-					category="就學經歷"
-					:order="index + 1"
-					@cancel="editToCreate"
-				/>
-			</div>
+			<ReviewState
+				v-if="item.state === 1"
+				category="就學經歷"
+				:itemName="item.name"
+				:order="index + 1"
+				@edit="reviewToEdit"
+			/>
+			<EditState
+				v-else-if="item.state === 2"
+				category="就學經歷"
+				:itemName="item.name"
+				:order="index + 1"
+				@cancel="editToReview"
+			/>
+			<CreateState
+				v-else-if="item.state === 3"
+				category="就學經歷"
+				:order="index + 1"
+				@edit="createToEdit"
+			/>
+			<EditState
+				v-else-if="item.state === 4"
+				category="就學經歷"
+				:order="index + 1"
+				@cancel="editToCreate"
+			/>
 		</div>
 
 		<!-- DIVIDER -->
@@ -49,36 +45,32 @@
 
 		<!-- EXAM AND QUALIFICATION TEST SCORE -->
 		<div v-for="(item, index) in examCertificateList" :key="index">
-			<div v-if="item.state === 1">
-				<ReviewState
-					category="考試與檢定分數"
-					:itemName="item.name"
-					:order="index + 1"
-					@edit="reviewToEdit"
-				/>
-			</div>
-			<div v-else-if="item.state === 2">
-				<EditState
-					category="考試與檢定分數"
-					:itemName="item.name"
-					:order="index + 1"
-					@cancel="editToReview"
-				/>
-			</div>
-			<div v-else-if="item.state === 3">
-				<CreateState
-					category="考試與檢定分數"
-					:order="index + 1"
-					@edit="createToEdit"
-				/>
-			</div>
-			<div v-else-if="item.state === 4">
-				<EditState
-					category="考試與檢定分數"
-					:order="index + 1"
-					@cancel="editToCreate"
-				/>
-			</div>
+			<ReviewState
+				v-if="item.state === 1"
+				category="考試與檢定分數"
+				:itemName="item.name"
+				:order="index + 1"
+				@edit="reviewToEdit"
+			/>
+			<EditState
+				v-else-if="item.state === 2"
+				category="考試與檢定分數"
+				:itemName="item.name"
+				:order="index + 1"
+				@cancel="editToReview"
+			/>
+			<CreateState
+				v-else-if="item.state === 3"
+				category="考試與檢定分數"
+				:order="index + 1"
+				@edit="createToEdit"
+			/>
+			<EditState
+				v-else-if="item.state === 4"
+				category="考試與檢定分數"
+				:order="index + 1"
+				@cancel="editToCreate"
+			/>
 		</div>
 
 		<!-- DIVIDER -->
@@ -92,36 +84,32 @@
 
 		<!-- OTHER -->
 		<div v-for="(item, index) in otherList" :key="index">
-			<div v-if="item.state === 1">
-				<ReviewState
-					category="其他有利於審查資料"
-					:itemName="item.name"
-					:order="index + 1"
-					@edit="reviewToEdit"
-				/>
-			</div>
-			<div v-else-if="item.state === 2">
-				<EditState
-					category="其他有利於審查資料"
-					:itemName="item.name"
-					:order="index + 1"
-					@cancel="editToReview"
-				/>
-			</div>
-			<div v-else-if="item.state === 3">
-				<CreateState
-					category="其他有利於審查資料"
-					:order="index + 1"
-					@edit="createToEdit"
-				/>
-			</div>
-			<div v-else-if="item.state === 4">
-				<EditState
-					category="其他有利於審查資料"
-					:order="index + 1"
-					@cancel="editToCreate"
-				/>
-			</div>
+			<ReviewState
+				v-if="item.state === 1"
+				category="其他有利於審查資料"
+				:itemName="item.name"
+				:order="index + 1"
+				@edit="reviewToEdit"
+			/>
+			<EditState
+				v-else-if="item.state === 2"
+				category="其他有利於審查資料"
+				:itemName="item.name"
+				:order="index + 1"
+				@cancel="editToReview"
+			/>
+			<CreateState
+				v-else-if="item.state === 3"
+				category="其他有利於審查資料"
+				:order="index + 1"
+				@edit="createToEdit"
+			/>
+			<EditState
+				v-else-if="item.state === 4"
+				category="其他有利於審查資料"
+				:order="index + 1"
+				@cancel="editToCreate"
+			/>
 		</div>
 
 		<!-- DIVIDER -->
@@ -130,7 +118,6 @@
 		<div class="mt-32px"></div>
 	</div>
 </template>
-
 <script setup lang="ts">
 import { reactive } from "vue";
 import ReviewState from "./components/reviewState.vue";
@@ -140,46 +127,60 @@ import ParagraphDivider from "../../../../../styles/paragraphDivider.vue";
 import "primeicons/primeicons.css";
 import AttachmentList from "./attachmentList.json";
 
+interface BasicData {
+	name?: string;
+	file_name?: string;
+	file_url?: string;
+	order?: number;
+	state?: number;
+}
+
 const props = defineProps(["userId"]);
 console.log(props.userId, "attachment info tab");
 
 const schoolExpList = reactive(
 	AttachmentList.school_exp?.map((item, index) => {
-		return { ...item, order: index, state: 1 };
+		const element: BasicData = {
+			...(item as object),
+			order: index,
+			state: 1,
+		};
+		return element;
 	})
 );
 schoolExpList.push({
 	order: schoolExpList.length,
 	state: 3,
-	name: "",
-	file_name: "",
-	file_url: "",
 });
 
 const examCertificateList = reactive(
 	AttachmentList.exam_score?.map((item, index) => {
-		return { ...item, order: index, state: 1 };
+		const element: BasicData = {
+			...(item as object),
+			order: index,
+			state: 1,
+		};
+		return element;
 	})
 );
 examCertificateList.push({
-	order: schoolExpList.length,
+	order: examCertificateList.length,
 	state: 3,
-	name: "",
-	file_name: "",
-	file_url: "",
 });
 
 const otherList = reactive(
 	AttachmentList.other?.map((item, index) => {
-		return { ...item, order: index, state: 1 };
+		const element: BasicData = {
+			...(item as object),
+			order: index,
+			state: 1,
+		};
+		return element;
 	})
 );
 otherList.push({
-	order: schoolExpList.length,
+	order: otherList.length,
 	state: 3,
-	name: "",
-	file_name: "",
-	file_url: "",
 });
 /* 判斷總共有幾個attachment 並對個別attachment宣告一個state去切換該用review state 或是 edit state 或是 create state */
 /* 方法：api讀進來，如果後端沒有宣告state的話跑回圈並對每個attachment新增 { state: 1 or 2 or 3 or 4 } */
