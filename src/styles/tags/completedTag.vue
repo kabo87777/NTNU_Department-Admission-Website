619e0ff0243a9a1d4f0c35327a21bff28f7f1361
PTLee70 <40847057s@gapps.ntnu.edu.tw>
2022-10-26 12:27:43 +0000

    feat(userSetting.vue)

---
+++ src/styles/tags/completedTag.vue
@@ -0,0 +1,17 @@
+<template id="completedTag">
+	<div class="completedTag">{{ $t("已完成") }}</div>
+</template>
+
+<style setup lang="css">
+.completedTag {
+	position: absolute;
+	right: 16px;
+	border-radius: 8px;
+	width: 80px;
+	height: 26px;
+	font-size: 12px;
+	padding-top: 4px;
+	border: 1px solid #62ae01;
+	color: #62ae01;
+}
+</style>
