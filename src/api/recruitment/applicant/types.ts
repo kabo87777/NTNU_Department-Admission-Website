6e9c04b17e92136cb1be7e7ad50659ac2ed328c9
PTLee70 <40847057s@gapps.ntnu.edu.tw>
2022-10-30 17:12:10 +0800

    feat: add APIs response type interface

--- src/api/recruitment/applicant/types.ts
+++ src/api/recruitment/applicant/types.ts
@@ -31,21 +31,56 @@ export interface RecruitmentApplicantAuthResponse {
 	image: any | null;
 }
 
-// TODO: disannotation while backend finished applicant get program API
-// export interface RecruitmentApplicantProgramListResponse {
-// 	id: number;
-// 	category: string;
-// 	name: string;
-// 	application_start_date: string;
-// 	application_end_date: string;
-// 	review_start_date: string;
-// 	review_end_date: string;
-// 	require_file: string; // Array or string ??
-// 	stage: string;
-// 	created_at: string;
-// 	updated_at: string;
-// 	applicant_required_info: null; // ??
-// 	applicant_required_file: null;
-// 	reviewer_required_info: null;
-// 	reviewer_required_file: null;
-// }
+// BELOW APIs not connected CURRENTLY
+export interface RecruitmentApplicantGetProgramNameResponse {
+	id: number;
+	category: string;
+	name: string;
+	application_start_date: string;
+	application_end_date: string;
+	review_start_date: string;
+	review_end_date: string;
+	require_file: string; // Array or string ??
+	stage: string;
+	created_at: string;
+	updated_at: string;
+	applicant_required_info: null; // ??
+	applicant_required_file: null;
+	reviewer_required_info: null;
+	reviewer_required_file: null;
+}
+
+export interface RecruitmentApplicantGetFileListResponse {
+	id: number;
+	category: string;
+	name: string;
+	filepath: object;
+	r_applicants_r_program_id: number;
+	created_at: string;
+	updated_at: string;
+}
+
+export interface RecruitmentApplicantGetUserInfoResponse {
+	id?: number;
+	name?: string;
+	email?: string;
+	national_id?: number;
+	sex?: string;
+	birth?: string;
+	day_phone?: string;
+	night_phone?: string;
+	mobile_phone?: string;
+	household_address?: string;
+	household_zipcode?: number | string;
+	communicate_address?: string;
+	communicate_zipcode?: number | string;
+	GSAT_num?: number | string;
+	GSAT_registration?: boolean;
+	graduated_school?: string;
+	graduated_major?: string;
+	isSameDept?: boolean;
+	isDisabled?: boolean;
+	r_applicant_id?: number;
+	created_at?: string;
+	updated_at?: string;
+}
