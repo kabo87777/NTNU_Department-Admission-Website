504deacc95b38b07102948b9533a43f1bc117ed4
Howard Guo <50100922+toto6038@users.noreply.github.com>
2022-11-07 11:39:53 +0800

    feat: add route for ListApplicant

---
+++ src/views/admission/manager/ListApplicant.vue
@@ -0,0 +1,165 @@
+<template>
+	<h1 class="text-4xl font-bold">{{ $t("申請者帳號設定") }}</h1>
+	<Divider />
+	<DataTable :value="tableData">
+		<template #empty>
+			<h2>{{ $t("尚無申請者帳號") }}</h2>
+		</template>
+
+		<Column field="id">
+			<template #header>{{ $t("申請者帳號") }}</template>
+		</Column>
+
+		<Column field="name">
+			<template #header>{{ $t("姓名") }}</template>
+		</Column>
+
+		<Column field="email">
+			<template #header>{{ $t("電子信箱") }}</template>
+		</Column>
+
+		<Column>
+			<template #header>{{ $t("操作") }}</template>
+			<template #body="slotProps">
+				<Button
+					icon="pi pi-pencil"
+					class="p-button-outlined p-button-success"
+					@click="openModal(slotProps.data)"
+				></Button>
+			</template>
+		</Column>
+	</DataTable>
+
+	<Dialog v-model:visible="modalVisible" :modal="true">
+		<template #header>
+			<h3 class="font-extrabold text-lg">
+				{{ $t("編輯申請者帳號") }}
+			</h3>
+		</template>
+
+		<template #default>
+			<div class="w-md">
+				<label for="" class="block font-black">{{
+					$t("帳號 ID")
+				}}</label>
+				<div>{{ modalData.id }}</div>
+
+				<div class="mt-3">
+					<label for="" class="block font-black">{{
+						$t("姓名")
+					}}</label>
+					<InputText
+						type="text"
+						class="w-full mt-1"
+						v-model:model-value="modalData.name"
+					></InputText>
+				</div>
+
+				<div class="mt-3">
+					<label for="" class="block font-black">{{
+						$t("電子信箱")
+					}}</label>
+					<InputText
+						type="email"
+						class="w-full mt-1"
+						v-model:model-value="modalData.email"
+					></InputText>
+				</div>
+
+				<div class="mt-3">
+					<label for="password" class="block font-black">{{
+						$t("密碼")
+					}}</label>
+					<InputText
+						type="password"
+						class="w-full mt-1"
+						v-model:model-value="modalData.passowrd"
+					></InputText>
+				</div>
+			</div>
+		</template>
+
+		<template #footer>
+			<div class="flex justify-center">
+				<div class="space-x-2">
+					<Button
+						icon="pi pi-check"
+						:label="$t('儲存')"
+						class="p-button-outlined p-button-success"
+						@click="saveChange"
+					></Button>
+					<Button
+						icon="pi pi-times"
+						:label="$t('取消')"
+						class="p-button-outlined p-button-danger"
+						@click="modalVisible = false"
+					></Button>
+				</div>
+			</div>
+		</template>
+	</Dialog>
+</template>
+
+<script setup lang="ts">
+import Button from "primevue/button";
+import Checkbox from "primevue/checkbox";
+import Column from "primevue/column";
+import DataTable from "primevue/datatable";
+import Dialog from "primevue/dialog";
+import Divider from "primevue/divider";
+import InputText from "primevue/inputtext";
+import { Ref, ref } from "vue";
+import {
+	useAdmissionAdminAuthStore,
+	useAdmissionReviewerAuthStore,
+} from "@/stores/universalAuth";
+import { AdmissionAdminAPI } from "@/api/admission/admin/api";
+import { useQuery } from "@tanstack/vue-query";
+import { InvalidSessionError } from "@/api/error"
+import { router } from "@/router";
+
+const adminAuth = useAdmissionAdminAuthStore()
+const api = new AdmissionAdminAPI(adminAuth)
+// NOTE: Copy and modified from SideBar.vue
+const {
+	isLoading,
+	isError,
+	data: applicants,
+	error,
+} = useQuery(["applicantList"], async () => {
+	try {
+		return await api.getApplicantList();
+	} catch (e: any) {
+		if (e instanceof InvalidSessionError) {
+			// FIXME: show session expiry notification??
+			// Why are we even here in the first place?
+			// MainContainer should have checked already.
+			console.error(
+				"Session has already expired while querying applicantList"
+			);
+			router.push("/");
+			return;
+		}
+	}
+});
+const modalVisible = ref(false);
+
+
+const tableData: Ref<OwO[]> = ref(mockData);
+const modalData: Ref<OwO> = ref({} as OwO);
+
+const openModal = (data) => {
+	modalData.value = data;
+	modalVisible.value = true;
+};
+
+const saveChange = () => {
+	let index = tableData.value.findIndex((x) => x.id === modalData.value.id);
+	console.log(index);
+	if (!isNaN(index)) {
+		tableData.value[index] = modalData.value;
+		console.log(tableData.value);
+	}
+	modalVisible.value = false;
+};
+</script>
