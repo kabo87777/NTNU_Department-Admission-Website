91ed85d2be6e376ac21e7e4f0d4e2822f7cd9053
HsuJinho <hsujinho@gmail.com>
2022-11-27 19:10:27 +0800

    fix

--- src/api/recruitment/reviewer/api.ts
+++ src/api/recruitment/reviewer/api.ts
@@ -16,36 +16,76 @@ export class RecruitmentReviewerAPI extends GenericAPI {
 		super(auth);
 	}
 
-	// TODO: disannotation while backend finished reviewer get program API
-	// async getProgramList(): Promise<RecruitmentReviewerProgramListResponse[]> {
-	// 	const data: APIGenericResponse = await this.instance.get(
-	// 		"/recruitment/reviewer/program"
-	// 	);
+	async getProgramList(): Promise<RecruitmentReviewerProgramListResponse[]> {
+		const data: APIGenericResponse = await this.instance.get(
+			"/recruitment/reviewer/program"
+		);
+
+		if (data.error === true || typeof data.data.programs === "undefined")
+			throw new Error("Failed to fetch program list");
+
+		return data.data.programs;
+	}
+
+	async changePassword(
+        body: object
+    ): Promise<RecruitmentReviewerGenericResponse> {
+        const data: APIGenericResponse = await this.instance.patch(
+            "recruitment/auth/reviewer/password",
+            body
+        );
+
+        if (data.error !== false) {
+            return {
+                success: false,
+                message: data.message.full_messages,
+            };
+        }
+
+        return {
+            success: true,
+            message: data.message,
+        };
+	}
+
+	async getApplicantList(
+		programID: number
+	): Promise<RecruitmentReviewerApplicantListResponse[]> {
+		const data: APIGenericResponse = await this.instance.get(
+			`/recruitment/reviewer/program/${programID}/applicant?detail=true`
+		);
+
+		if (data.error === true || typeof data.data.applicants === "undefined")
+			throw new Error("Failed to fetch applicant list");
+
+		return data.data.applicants;
+	}
+
+	async getApplicantComment(
+		programID: number,
+		applicantID: string | string[]
+	): Promise<RecruitmentReviewerApplicantCommentResponse> {
+		const data: APIGenericResponse = await this.instance.get(
+			`/recruitment/reviewer/program/${programID}/applicant/${applicantID}/comment`
+		);
+
+		if (data.error === true || typeof data.data === "undefined")
+			throw new Error("Failed to fetch applicant comment");
+
+		return data.data;
+	}
+
+	async getApplicantInfo(
+		programID: number,
+		applicantID: string | string[]
+	): Promise<RecruitmentReviewerApplicantInfoResponse> {
+		const data: APIGenericResponse = await this.instance.get(
+			`/recruitment/reviewer/program/${programID}/applicant/${applicantID}/info`
+		);
 
 		if (data.error === true || typeof data.data === "undefined")
 			throw new Error("Failed to fetch applicant info");
 
 		return data.data;
-	}
-
-	async changePassword(
-		body: object
-	): Promise<RecruitmentReviewerGenericResponse> {
-		const data: APIGenericResponse = await this.instance.patch(
-			"recruitment/auth/reviewer/password",
-			body
-		);
-
-		if (data.error !== false) {
-			return {
-				success: false,
-				message: data.message.full_messages,
-			};
-		}
-
-		return {
-			success: true,
-			message: data.message,
-		};
 	}
 }
