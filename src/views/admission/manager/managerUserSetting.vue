5b0898d24cb2fd45442c3079d4d0943e6647f9f0
Renee0727 <edntrz0727@gmail.com>
2022-12-05 23:27:36 +0000

    chore: remove manager usersetting buttons

--- src/views/admission/manager/managerUserSetting.vue
+++ src/views/admission/manager/managerUserSetting.vue
@@ -2,51 +2,9 @@
 	<div class="ml-50px mt-50px">
 		<div>{{ $t("使用者管理") }}</div>
 		<div class="bigRedDivider"></div>
-		<div class="mt-20px">
-			{{ $t("帳號名稱") }}：{{ userId }}
-			<Button @click="editUserId" class="p-button-outlined">
-				<img src="/assets/UserSetting/Edit.png" />
-				{{ $t("修改") }}
-			</Button>
-			<Dialog header="修改名稱" v-model:visible="displayUserId">
-				<div>{{ $t("輸入新名稱") }}</div>
-				<InputText type="text" v-model="userId"></InputText>
-				<div class="mt-10px">
-					<Button @click="ensureUserId">{{ $t("確認修改") }}</Button>
-					<Button @click="cancelUserId">{{ $t("取消") }}</Button>
-				</div>
-			</Dialog>
-		</div>
-		<div class="mt-20px">
-			{{ $t("聯絡信箱") }}：{{ email }}
-			<Button @click="editEmail" class="p-button-outlined">
-				<img src="/assets/UserSetting/Edit.png" />
-				{{ $t("修改") }}
-			</Button>
-			<Dialog header="修改信箱" v-model:visible="displayEmail">
-				<div>{{ $t("輸入新信箱") }}</div>
-				<InputText type="text" v-model="email"></InputText>
-				<div class="mt-10px">
-					<Button @click="ensureEmail">{{ $t("確認修改") }}</Button>
-					<Button @click="cancelEmail">{{ $t("取消") }}</Button>
-				</div>
-			</Dialog>
-		</div>
-		<div class="mt-20px">
-			{{ $t("手機號碼") }}：{{ phone }}
-			<Button @click="editPhone" class="p-button-outlined">
-				<img src="/assets/UserSetting/Edit.png" />
-				{{ $t("修改") }}
-			</Button>
-			<Dialog header="修改信箱" v-model:visible="displayPhone">
-				<div>{{ $t("輸入新號碼") }}</div>
-				<InputText type="text" v-model="phone"></InputText>
-				<div class="mt-10px">
-					<Button @click="ensurePhone">{{ $t("確認修改") }}</Button>
-					<Button @click="cancelPhone">{{ $t("取消") }}</Button>
-				</div>
-			</Dialog>
-		</div>
+		<div class="mt-20px">{{ $t("帳號名稱") }}：{{ userId }}</div>
+		<div class="mt-20px">{{ $t("聯絡信箱") }}：{{ email }}</div>
+		<div class="mt-20px">{{ $t("手機號碼") }}：{{ phone }}</div>
 		<Divider />
 		<div class="mt-20px font-[500] font-bold text-[24px] flex">
 			<div>{{ $t("修改密碼") }}</div>
@@ -120,57 +78,18 @@
 <script setup lang="ts">
 import Button from "primevue/button";
 import InputText from "primevue/inputtext";
-import { ref, reactive, toRaw, onMounted } from "vue";
-import Dialog from "primevue/dialog";
+import { ref, reactive, toRaw } from "vue";
 import { useAdmissionAdminAuthStore } from "@/stores/universalAuth";
 import { AdmissionAdminAPI } from "@/api/admission/admin/api";
 import { InvalidSessionError } from "@/api/error";
 import { useToast } from "primevue/usetoast";
-// import { AdmissionManagerAuthResponse } from "@/api/admission/admin/types";
-// import { useUserInfoStore } from "@/stores/AdmissionAdminStore";
 
 const toast = useToast();
 const userId = ref("系辦主管");
-const displayUserId = ref();
-const editUserId = () => {
-	displayUserId.value = true;
-};
-const ensureUserId = () => {
-	displayUserId.value = false;
-};
-const cancelUserId = () => {
-	displayUserId.value = false;
-};
-
 const email = ref("csie_office@ntnu.edu.tw");
-const displayEmail = ref();
-const editEmail = () => {
-	displayEmail.value = true;
-};
-const ensureEmail = () => {
-	displayEmail.value = false;
-};
-const cancelEmail = () => {
-	displayEmail.value = false;
-};
-
 const phone = ref("0912345678");
-const displayPhone = ref();
-const editPhone = () => {
-	displayPhone.value = true;
-};
-const ensurePhone = () => {
-	displayPhone.value = false;
-};
-const cancelPhone = () => {
-	displayPhone.value = false;
-};
-
 const adminAuth = useAdmissionAdminAuthStore();
-// const adminStore = useUserInfoStore();
 const api = new AdmissionAdminAPI(adminAuth);
-
-// const adminInfo: AdmissionManagerAuthResponse = toRaw(adminStore.userInfo);
 
 const initialPassValue = {
 	isCurrentPassBlank: false,
@@ -282,6 +201,4 @@ const handleSubmit = async () => {
 		});
 	}
 };
-
-//onMounted(() => {});
 </script>
