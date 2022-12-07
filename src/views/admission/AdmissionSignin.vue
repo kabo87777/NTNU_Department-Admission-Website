d360a13e7ea4ee05c03c73dfed3f039986ff9f7d
flyingdollar.cheng <flyingdollar.cheng@gmail.com>
2022-12-07 18:33:38 +0800

    <feat>(admission/login):畫面調整為 RWD

--- src/views/admission/AdmissionSignin.vue
+++ src/views/admission/AdmissionSignin.vue
@@ -1,83 +1,99 @@
 <template>
-	<div class="grid grid-cols-2 gap-15px">
-		<div>
-			<img src="/assets/login-page/Login-img.png" class="h-screen" />
-		</div>
-		<div class="m-auto">
-			<div class="flex m-auto h-15">
-				<div>
-					<img
-						src="/assets/login-page/NTNU-logo-B1.png"
-						class="h-15"
-					/>
-				</div>
-				<Divider layout="vertical" />
-				<div class="mt-1.5">
-					<div class="text-4xl font-bold text-gray-500">
-						資訊工程學系
+	<div class="fixed inset-y-0 left-0">
+		<img src="/assets/login-page/Login-img.png" class="h-200" />
+	</div>
+	<!-- FIXME:附著在底下的空白，以免圖片露餡，但或許有更好的做法 -->
+	<div
+		class="fixed inset-y-0 right-0 z-30 bg-white"
+		w="125 <sm:full lg:150"
+	/>
+	<div
+		class="absolute inset-y-0 right-0 z-50 bg-white"
+		w="125 <sm:full lg:150"
+		px="lg:15"
+	>
+		<div class="flex flex-col py-4 gap-6 mx-8 h-full">
+			<!-- 標題內容 -->
+			<div space="y-2">
+				<img
+					src="/assets/login-page/NTNU-logo-B1.png"
+					w="48"
+					class="sm:invisible"
+				/>
+				<!-- 返回按鈕 -->
+				<router-link to="mainpage">
+					<button
+						class="flex items-center gap-2 px-2 py-2 mt-5 mb-3"
+						bg="hover:nGrey-200 active:nGrey-600"
+						text="sm secondary hover:title active:white"
+						border="rounded-lg"
+					>
+						<i class="pi pi-angle-left" />
+						<div>選擇其他系統</div>
+						<div>Select other system</div>
+					</button>
+				</router-link>
+				<div class="mx-8" space="y-2">
+					<div text="3xl title" font="medium">
+						資訊工程學系招生系統
 					</div>
-					<div class="text-xs text-gray-400">
-						Department of Computer Science and Information
-						Enginering
+					<div text="lg title" font="medium">
+						NTNU CSIE Admissions
 					</div>
 				</div>
 			</div>
-			<router-link to="mainpage">
-				<button
-					class="flex items-center gap-2 px-2 py-2 mt-5 mb-3"
-					bg="transparent hover:gray-100"
-					text="sm gray-400 hover:gray-600"
-					border="rounded"
-				>
-					<i class="pi pi-angle-left" />
-					<div>選擇其他系統</div>
-					<div>Select other system</div>
-				</button>
-			</router-link>
-			<div>
-				<div class="mt-25px ml-204px text-4xl font-bold text-gray-500">
-					{{ $t("資訊工程學系報名系統") }}
+
+			<Divider align="center">
+				<div class="px-4 font-light" text="body sm">
+					選擇身份 Select your identity
 				</div>
-				<div class="ml-204px text-xs text-gray-500">
-					NTNU CSIE Admissions
+			</Divider>
+
+			<!-- 按鈕選項 -->
+			<div class="w-4/5 mx-auto" space="y-8">
+				<div class="flex-auto place-items-center">
+					<router-link to="/admission/applicant/signin">
+						<button
+							class="p-4 w-full"
+							bg="hover:nGold-300 active:nGold-500"
+							border="2 opacity-30 nGold-600 rounded-lg hover:nGold-300"
+							text="pApplicant hover:title active:white"
+						>
+							<div>申請者登入 Applicant Login</div>
+						</button>
+					</router-link>
 				</div>
-			</div>
-			<div class="mt-50px ml-184px userLogin">
-				<router-link to="/admission/applicant/signin">
-					<button
-						class="text-xl h-90px w-420px md:mx-auto w-full py-4 applicantButtonStyle"
-						border="2  rounded-lg"
-					>
-						<div>申請者登入 Applicant Login</div>
-					</button>
-				</router-link>
-			</div>
-			<div class="mt-50px ml-184px userLogin">
-				<router-link to="/admission/reviewer/signin">
-					<button
-						class="text-xl h-90px w-420px md:mx-auto w-full py-4 reviewerButtonStyle"
-						border="2  rounded-lg"
-					>
-						<div>審查委員登入 Reviewer Login</div>
-					</button>
-				</router-link>
-			</div>
-			<div class="mt-50px ml-184px">
-				<router-link to="/admission/manager/signin">
-					<button
-						class="text-xl h-90px w-420px md:mx-auto w-full py-4 managerButtonStyle"
-						border="2  rounded-lg"
-					>
-						<div>行政人員登入 Manager Login</div>
-					</button>
-				</router-link>
+				<div class="flex-auto place-items-center">
+					<router-link to="/admission/reviewer/signin">
+						<button
+							class="p-4 w-full"
+							bg="hover:nBlue-200 active:nBlue-500"
+							border="2 opacity-30 nBlue-600 rounded-lg hover:nBlue-200"
+							text="pReviewer hover:title active:white"
+						>
+							<div>審查委員登入 Reviewer Login</div>
+						</button>
+					</router-link>
+				</div>
+				<div class="flex-auto place-items-center">
+					<router-link to="/admission/manager/signin">
+						<button
+							class="p-4 w-full"
+							bg="hover:nRed-200 active:nRed-500"
+							border="2 opacity-30 nRed-600 rounded-lg hover:nRed-200"
+							text="pAdmin hover:title active:white"
+						>
+							<div>行政人員登入 Manager Login</div>
+						</button>
+					</router-link>
+				</div>
 			</div>
 			<!-- 通往推薦信作業身份驗證的神秘按鈕 -->
 			<!-- <div>
-				<router-link to="/admission/recommenderAuthVerify">
-					<Button>借放一下</Button>
-				</router-link>
-			</div> -->
+					<router-link to="/admission/recommenderAuthVerify">
+						<Button>借放一下</Button>
+					</router-link>
+				</div> -->
 		</div>
 	</div>
 </template>
