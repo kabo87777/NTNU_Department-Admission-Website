c321c9440309886ac2c3224f9154ad48a0599f42
flyingdollar.cheng <flyingdollar.cheng@gmail.com>
2022-12-09 12:28:39 +0800

    <feat>(Regist):Fixed Regist UI.

--- src/views/recruitment/applicant/login/regist.vue
+++ src/views/recruitment/applicant/login/regist.vue
@@ -1,136 +1,111 @@
 <template>
-	<!-- Background Image -->
-	<img
-		src="/assets/login-page/Login-img.png"
-		class="fixed min-h-screen h-200"
-	/>
-	<!-- White Background -->
-	<div
-		class="fixed h-screen z-1 right-0 bg-white"
-		w="<md:screen md:min-112 screen-4/9"
-	/>
-	<!-- Content -->
-	<div
-		class="absolute h-screen z-50 right-0 bg-white"
-		w="<md:screen md:min-112 screen-4/9"
-	>
-		<div flex="~ col" mx="auto" w="100" h="screen">
-			<div my="auto">
-				<!-- Button: Back to MainPage -->
-				<router-link to="/recruitment/applicant/signin">
-					<button
-						class="flex items-center gap-2 p-2 my-4"
-						text="sm secondary"
-						border="rounded-lg"
-						hover="bg-nGrey-200 text-title"
-						active="bg-nGrey-600 text-white"
-					>
-						<i class="pi pi-angle-left" />
-						<div>回到登入畫面</div>
-						<div>Back to login page</div>
-					</button>
-				</router-link>
-				<div mx="4" space="y-2">
-					<div text="secondary" font="light">
-						國立臺灣師範大學資訊工程學系
-					</div>
-					<div text="3xl title" font="medium">教師聘請系統</div>
-					<div text="lg title" font="medium">
-						Teacher Recruitment System
-					</div>
+	<div my="auto">
+		<!-- Button: Back to Applicant Signin -->
+		<router-link to="/recruitment/applicant/signin">
+			<button
+				class="flex items-center gap-2 p-2 my-4"
+				text="sm secondary"
+				border="rounded-lg"
+				hover="bg-nGrey-200 text-title"
+				active="bg-nGrey-600 text-white"
+			>
+				<i class="pi pi-angle-left" />
+				<div>回到登入畫面</div>
+				<div>Back to login page</div>
+			</button>
+		</router-link>
+		<div mx="4" space="y-2">
+			<div text="secondary" font="light">
+				國立臺灣師範大學資訊工程學系
+			</div>
+			<div text="3xl title" font="medium">教師聘請系統</div>
+			<div text="lg title" font="medium">Teacher Recruitment System</div>
+		</div>
+		<!-- Divider -->
+		<Divider align="center">
+			<div p="4" text="sm pApplicant">申請者註冊 Applicant Regist</div>
+		</Divider>
+		<!-- Regist Form -->
+		<div flex="~ col gap-6" w="3/4" mx="auto">
+			<!-- Username -->
+			<div flex="~ col gap-1" text="sm body" font="light">
+				<div>輸入使用者名稱 Enter Username</div>
+				<InputText
+					name="username"
+					type="text"
+					v-model="userRegistData.name"
+					class="h-11"
+					:class="{ 'p-invalid': password.isNameBlank }"
+					required
+				/>
+				<div text="xs danger" v-if="password.isNameBlank">
+					※ 必須輸入使用者名稱 Username is required.
 				</div>
-				<!-- Divider -->
-				<Divider align="center">
-					<div p="4" text="sm pApplicant">
-						申請者註冊 Applicant Regist
-					</div>
-				</Divider>
-				<!-- Regist Form -->
-				<div flex="~ col gap-6" w="3/4" mx="auto">
-					<!-- Username -->
-					<div flex="~ col gap-1" text="sm body" font="light">
-						<div>輸入使用者名稱 Enter Username</div>
-						<InputText
-							name="username"
-							type="text"
-							v-model="userRegistData.name"
-							class="h-11"
-							:class="{ 'p-invalid': password.isNameBlank }"
-							required
-						/>
-						<div text="xs danger" v-if="password.isNameBlank">
-							※ 必須輸入使用者名稱 Username is required.
-						</div>
-					</div>
-					<!-- E-mail -->
-					<div flex="~ col gap-1" text="sm body" font="light">
-						<div>設置電郵 Setting E-mail</div>
-						<InputText
-							name="email"
-							type="email"
-							v-model="userRegistData.email"
-							class="h-11"
-							:class="{ 'p-invalid': password.isNameBlank }"
-							required
-						/>
-						<div text="xs danger" v-if="password.isEmailBlank">
-							※ 必須輸入輸入電郵 E-mail is required.
-						</div>
-					</div>
+			</div>
+			<!-- E-mail -->
+			<div flex="~ col gap-1" text="sm body" font="light">
+				<div>設置電郵 Setting E-mail</div>
+				<InputText
+					name="email"
+					type="email"
+					v-model="userRegistData.email"
+					class="h-11"
+					:class="{ 'p-invalid': password.isNameBlank }"
+					required
+				/>
+				<div text="xs danger" v-if="password.isEmailBlank">
+					※ 必須輸入輸入電郵 E-mail is required.
+				</div>
+			</div>
 
-					<div flex="~ col gap-1" text="sm body" font="light">
-						<div>設置密碼 Setting Password</div>
-						<InputText
-							name="password"
-							type="password"
-							v-model="userRegistData.password"
-							class="h-11"
-							:class="{
-								'p-invalid': password.isCurrentPassBlank,
-							}"
-							required
-						/>
-						<div
-							text="xs danger"
-							v-if="password.isCurrentPassBlank"
-						>
-							※ 請輸入新密碼 Please set your new password.
-						</div>
-					</div>
-					<div flex="~ col gap-1" text="sm body" font="light">
-						<div>確認密碼 Confirm Password</div>
-						<InputText
-							name="password"
-							type="password"
-							v-model="userRegistData.password_confirmation"
-							class="h-11"
-							:class="{ 'p-invalid': password.notMatch }"
-							required
-						/>
-						<div text="xs danger" v-if="password.notMatch">
-							※ 密碼不符 Confirm password is not matched.
-						</div>
-					</div>
+			<div flex="~ col gap-1" text="sm body" font="light">
+				<div>設置密碼 Setting Password</div>
+				<InputText
+					name="password"
+					type="password"
+					v-model="userRegistData.password"
+					class="h-11"
+					:class="{
+						'p-invalid': password.isCurrentPassBlank,
+					}"
+					required
+				/>
+				<div text="xs danger" v-if="password.isCurrentPassBlank">
+					※ 請輸入新密碼 Please set your new password.
 				</div>
-				<!-- Regist Button -->
-				<div flex="~ col" w="60" m="x-auto t-8 b-auto">
-					<button
-						class="p-2 w-full border-2 text-pApplicant"
-						border="2 opacity-30 nGold-500 rounded-lg"
-						hover="text-title bg-nGold-300 border-nGold-300"
-						active="text-white bg-nGold-500"
-						:loading="isRegistLoading"
-						@click="handleSubmit"
-					>
-						<div class="flex justify-center gap-2 mx-auto">
-							<div>註冊</div>
-							<div>Register</div>
-						</div>
-					</button>
-					<div>
-						<Turnstile ref="turnstileRef" />
-					</div>
+			</div>
+			<div flex="~ col gap-1" text="sm body" font="light">
+				<div>確認密碼 Confirm Password</div>
+				<InputText
+					name="password"
+					type="password"
+					v-model="userRegistData.password_confirmation"
+					class="h-11"
+					:class="{ 'p-invalid': password.notMatch }"
+					required
+				/>
+				<div text="xs danger" v-if="password.notMatch">
+					※ 密碼不符 Confirm password is not matched.
 				</div>
+			</div>
+		</div>
+		<!-- Regist Button -->
+		<div flex="~ col" w="60" m="x-auto t-8 b-auto">
+			<button
+				class="p-2 w-full border-2 text-pApplicant"
+				border="2 opacity-30 nGold-500 rounded-lg"
+				hover="text-title bg-nGold-300 border-nGold-300"
+				active="text-white bg-nGold-500"
+				:loading="isRegistLoading"
+				@click="handleSubmit"
+			>
+				<div class="flex justify-center gap-2 mx-auto">
+					<div>註冊</div>
+					<div>Register</div>
+				</div>
+			</button>
+			<div>
+				<Turnstile ref="turnstileRef" />
 			</div>
 		</div>
 	</div>
@@ -246,24 +221,3 @@ const handleSubmit = async () => {
 	}
 };
 </script>
-
-<style setup lang="css">
-.applicantTextColor {
-	color: #736028;
-}
-.applicantButtonStyle {
-	background-color: #fafac7;
-	border-color: #dbd379;
-	color: #544830;
-}
-.applicantButtonStyle:hover {
-	background-color: #8a7b27;
-	border-color: #8a7b27;
-	color: white;
-}
-.applicantButtonStyle:active {
-	background-color: #624f2a;
-	border-color: #624f2a;
-	color: white;
-}
-</style>
--- src/views/recruitment/applicant/login/registDone.vue
+++ src/views/recruitment/applicant/login/registDone.vue
@@ -1,75 +1,49 @@
 <template>
-	<div class="fixed top-0 left-0">
-		<img
-			src="/assets/login-page/Login-img.png"
-			class="min-h-screen h-200"
-		/>
-	</div>
-	<div
-		class="fixed inset-y-0 right-0 z-30 bg-white"
-		w="125 <sm:full lg:150"
-	/>
-	<div
-		class="absolute inset-y-0 right-0 z-50 bg-white"
-		w="125 <sm:full lg:150"
-		px="lg:15"
-	>
-		<div class="flex flex-col py-4 gap-6 mx-8 h-full">
-			<!-- 標題內容 -->
-			<div space="y-2">
-				<img
-					src="/assets/login-page/NTNU-logo-B1.png"
-					w="48"
-					class="sm:invisible"
-				/>
-				<!-- 返回按鈕 -->
-				<router-link to="/recruitment/applicant/signin">
-					<button
-						class="flex items-center gap-2 px-2 py-2 mt-5 mb-3"
-						bg="hover:nGrey-200 active:nGrey-600"
-						text="sm secondary hover:title active:white"
-						border="rounded-lg"
-					>
-						<i class="pi pi-angle-left" />
-						<div>返回登入頁面</div>
-						<div>Back to Login</div>
-					</button>
-				</router-link>
-				<div class="mx-8" space="y-2">
-					<div text="3xl title" font="medium">教師聘請系統</div>
-					<div text="lg title" font="medium">
-						Teacher Recruitment System
-					</div>
-				</div>
-			</div>
-
-			<Divider align="center">
-				<div class="px-4 font-light" text="sm pApplicant">
-					申請者註冊 Applicant Register
-				</div>
-			</Divider>
-
-			<!-- 註冊成功 -->
-			<div class="m-auto" space="y-4" text="success lg">
-				<div class="flex w-full justify-center">
-					<i class="pi pi-check-circle" style="font-size: 4rem" />
-				</div>
-				<div>註冊成功 Successfull</div>
-			</div>
-			<!-- 確認信箱訊息 -->
-			<div
-				class="flex justify-center items-center p-6 gap-6 my-6"
-				text="secondary"
-				bg="nGrey-50"
-				border="rounded-xl"
+	<div my="auto">
+		<!-- Button: Back to Applicant Signin -->
+		<router-link to="/recruitment/applicant/signin">
+			<button
+				class="flex items-center gap-2 p-2 my-4"
+				text="sm secondary"
+				border="rounded-lg"
+				hover="bg-nGrey-200 text-title"
+				active="bg-nGrey-600 text-white"
 			>
-				<i class="pi pi-envelope" style="font-size: 2rem" />
-				<div class="space-y-1" text="xs">
-					<div>請至郵箱啟動帳號，並返回登入畫面</div>
-					<div>
-						Please Check your mail in order to active your account,
-						then turn back to Login page.
-					</div>
+				<i class="pi pi-angle-left" />
+				<div>回到登入畫面</div>
+				<div>Back to login page</div>
+			</button>
+		</router-link>
+		<div mx="4" space="y-2">
+			<div text="secondary" font="light">
+				國立臺灣師範大學資訊工程學系
+			</div>
+			<div text="3xl title" font="medium">教師聘請系統</div>
+			<div text="xl title" font="medium">Teacher Recruitment System</div>
+		</div>
+		<!-- Divider -->
+		<Divider align="center">
+			<div p="4" text="sm pApplicant">申請者註冊 Applicant Regist</div>
+		</Divider>
+		<!-- Regist Successful -->
+		<div flex="~ col" text="success lg" class="items-center" py="8">
+			<i class="pi pi-check-circle" m="b-4 r-2" style="font-size: 4rem" />
+			<div>註冊成功</div>
+			<div>Successfull</div>
+		</div>
+		<!-- Info: Check E-mail -->
+		<div
+			class="flex justify-center items-center p-6 gap-6 my-6"
+			text="secondary"
+			bg="nGrey-50"
+			border="rounded-xl"
+		>
+			<i class="pi pi-envelope" style="font-size: 2rem" />
+			<div class="space-y-1" text="xs">
+				<div>請至郵箱啟動帳號，並返回登入畫面</div>
+				<div>
+					Please Check your mail in order to active your account, then
+					turn back to Login page.
 				</div>
 			</div>
 		</div>
