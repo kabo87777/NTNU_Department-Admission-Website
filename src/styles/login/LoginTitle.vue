6de9ad5fdc1827058af8b4c17dd97dc0d18a5599
flyingdollar.cheng <flyingdollar.cheng@gmail.com>
2022-12-13 01:03:43 +0800

    <style>(style/login): Set Login Title & Button Component

---
+++ src/styles/login/LoginTitle.vue
@@ -0,0 +1,22 @@
+<template>
+	<!-- Title -->
+	<div mx="8" space="y-2">
+		<div text="lg secondary">
+			<slot name="Subtitle" />
+		</div>
+		<div text="4xl title" font="medium">
+			<slot name="Chinese" />
+		</div>
+		<div text="2xl title" font="medium">
+			<slot name="English" />
+		</div>
+	</div>
+	<!-- Divider -->
+	<div mx="12">
+		<Divider align="center">
+			<div p="x-4 y-6" text="secondary">
+				<slot name="Divider" />
+			</div>
+		</Divider>
+	</div>
+</template>
