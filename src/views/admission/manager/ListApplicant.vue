707c256439d79d5100fab9f49f39a2eda54e8855
Howard Guo <4442713-toto6038@users.noreply.gitlab.com>
2022-12-21 17:25:40 +0800

    chore(admission admin): remove edit applicant button and dialog modal

--- src/views/admission/manager/ListApplicant.vue
+++ src/views/admission/manager/ListApplicant.vue
@@ -90,12 +90,6 @@
 					<template #header>{{ $t("操作") }}</template>
 					<template #body="slotProps">
 						<span class="flex gap-x-3">
-							<!-- Edit button -->
-							<NButton
-								Admin
-								icon="pi pi-pencil"
-								@click="openModal(slotProps.data)"
-							/>
 							<!-- Delete button -->
 							<NButton
 								Danger
@@ -114,102 +108,24 @@
 			</DataTable>
 		</template>
 	</Layout>
-	<!-- 編輯彈跳視窗 -->
-	<Dialog v-model:visible="modalVisible" :modal="true">
-		<template #header>
-			<h3 class="font-extrabold text-lg">
-				{{ $t("編輯申請者帳號") }}
-			</h3>
-		</template>
-		<template #default>
-			<div class="w-md">
-				<label for="" class="block font-black">{{
-					$t("帳號 ID")
-				}}</label>
-				<div>{{ modalData.id }}</div>
-				<div class="mt-3">
-					<label for="" class="block font-black">{{
-						$t("姓名")
-					}}</label>
-					<InputText
-						type="text"
-						class="w-full mt-1"
-						v-model:model-value="modalData.name"
-						disabled="true"
-					></InputText>
-				</div>
-				<div class="mt-3">
-					<label for="" class="block font-black">{{
-						$t("電子信箱")
-					}}</label>
-					<InputText
-						type="email"
-						class="w-full mt-1"
-						v-model:model-value="modalData.email"
-						:disabled="true"
-					></InputText>
-				</div>
-
-				<div class="mt-3">
-					<label for="password" class="block font-black">{{
-						$t("密碼")
-					}}</label>
-					<Password
-						class="mt-1 w-full"
-						v-model="modalData.password"
-						:feedback="false"
-						:disabled="modalData.allow_password_change === false"
-						:toggle-mask="modalData.allow_password_change === true"
-						@focus.once="modalData.password = ''"
-						input-class="w-full"
-					/>
-				</div>
-			</div>
-		</template>
-		<template #footer>
-			<div class="flex justify-center">
-				<div class="space-x-2">
-					<Button
-						icon="pi pi-check"
-						:label="$t('儲存')"
-						class="p-button-outlined p-button-success"
-						@click="saveChange"
-					></Button>
-					<Button
-						icon="pi pi-times"
-						:label="$t('取消')"
-						class="p-button-outlined p-button-danger"
-						@click="modalVisible = false"
-					></Button>
-				</div>
-			</div>
-		</template>
-	</Dialog>
 	<ConfirmDialog />
 </template>
 
 <script setup lang="ts">
-import Button from "primevue/button";
 import NButton from "@/styles/CustomButton.vue";
 import Layout from "@/components/Layout.vue";
 import Column from "primevue/column";
 import DataTable from "primevue/datatable";
-import Dialog from "primevue/dialog";
-import Divider from "primevue/divider";
-import InputText from "primevue/inputtext";
-import Password from "primevue/password";
 
 import { computed, Ref, ref, toRaw, watch, watchEffect } from "vue";
 import { useAdmissionAdminAuthStore } from "@/stores/universalAuth";
 import { AdmissionAdminAPI } from "@/api/admission/admin/api";
 import { useMutation, useQuery } from "@tanstack/vue-query";
-import { InvalidSessionError } from "@/api/error";
 import { router } from "@/router";
 import { AdmissionAdminApplicantsListResponse } from "@/api/admission/admin/types";
 import { useGlobalStore } from "@/stores/globalStore";
 import FileUpload, { FileUploadBeforeUploadEvent } from "primevue/fileupload";
 import { FileUploadUploaderEvent } from "primevue/fileupload";
-import ProgressBar from "primevue/progressbar";
 import { useToast } from "primevue/usetoast";
 import { useI18n } from "vue-i18n";
 import ConfirmDialog from "primevue/confirmdialog";
@@ -256,52 +172,12 @@ const {
 const isImporting = ref(false);
 const isTableLoading = computed(() => isLoading.value || isImporting.value);
 
-interface modalForm {
-	id: number;
-	name: string;
-	email: string;
-	password: string;
-	allow_password_change: boolean;
-}
-
 globalStore.$subscribe((mutation, state) => {
 	if (state.program) {
 		console.log(state.program.category + state.program.name);
 	}
 	refetch();
 });
-
-const modalVisible = ref(false);
-const modalData = ref<modalForm>({} as modalForm);
-
-const openModal = (applicantData: AdmissionAdminApplicantsListResponse) => {
-	console.log(applicantData);
-	// Pick properties we need for form
-	const formData: modalForm = (({
-		id,
-		name,
-		email,
-		allow_password_change,
-	}: AdmissionAdminApplicantsListResponse) => ({
-		id,
-		name,
-		email,
-		// This is used to show dots as visual hint for user
-		// After user click the password input box, the value will be cleared
-		password: "JUST A PLACEHOLDER",
-		allow_password_change,
-	}))(applicantData);
-
-	modalData.value = formData;
-	modalVisible.value = true;
-};
-
-const saveChange = () => {
-	if (!tableData.value) return;
-	// TODO: send patch request to backend
-	api.updateApplicantData(modalData.value.id, modalData.value);
-	modalVisible.value = false;
-};
 
 const getTableItemQty = computed(() => {
 	return tableData.value ? tableData.value.length : 0;
