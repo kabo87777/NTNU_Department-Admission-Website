d00b2d00832961527f780c95ca0ff802d171f17e
40947010S <iamtim303@gmail.com>
2022-12-24 00:15:35 +0800

    feat():lint

--- src/views/admission/manager/applicantsUploadList/reviewTabs/basicInfo.vue
+++ src/views/admission/manager/applicantsUploadList/reviewTabs/basicInfo.vue
@@ -1,121 +1,128 @@
 <template>
-  <div class="grid">
-    <div>
-      <div class="mt-24px text-24px font-bold">{{ $t("姓名資訊") }}</div>
-      <div class="admApplicantBasicInfoCard">
-        <div style="color: #333333">
-          <div>{{ $t("中文姓氏") + $t(":") }}</div>
-          <div>{{ $t("中文名字") + $t(":") }}</div>
-          <div>{{ $t("英文姓氏") + $t(":") }}</div>
-          <div>{{ $t("英文中間名字") + $t(":") }}</div>
-          <div>{{ $t("英文名字") + $t(":") }}</div>
-        </div>
-        <div class="font-bold ml-12px">
-          <div v-if="userInfo?.cn_surname">{{ userInfo.cn_surname }}</div>
-          <div v-else>-</div>
-          <div v-if="userInfo?.name">{{ userInfo?.name }}</div>
-          <div v-else>-</div>
-          <div v-if="userInfo?.en_surname">
-            {{ userInfo?.en_surname }}
-          </div>
-          <div v-else>-</div>
-          <div v-if="userInfo?.en_midname">
-            {{ userInfo?.en_midname }}
-          </div>
-          <div v-else>-</div>
-          <div v-if="userInfo?.en_givenname">
-            {{ userInfo?.en_givenname }}
-          </div>
-          <div v-else>-</div>
-        </div>
-      </div>
-    </div>
-    <div>
-      <div class="mt-24px text-24px font-bold">
-        {{ $t("入學身分") }}
-      </div>
-      <div class="admApplicantBasicInfoCard">
-        <div style="color: #333333">
-          <div>{{ $t("入學身份") + $t(":") }}</div>
-          <div>{{ $t("國籍") + $t(":") }}</div>
-          <div>{{ $t("身份證字號") + $t(":") }}</div>
-          <div>{{ $t("居留證統一證號") + $t(":") }}</div>
-        </div>
-        <div class="font-bold ml-12px">
-          <div v-if="userInfo?.isForeigner">{{ $t("外籍生") }}</div>
-          <div v-else-if="userInfo?.isForeigner === false">
-            {{ $t("本地生") }}
-          </div>
-          <div v-else>-</div>
-          <div v-if="userInfo?.nationality">
-            {{ userInfo?.nationality }}
-          </div>
-          <div v-else>-</div>
-          <div v-if="userInfo?.national_id && userInfo?.isForeigner === false">
-            {{ userInfo.national_id }}
-          </div>
-          <div v-else>-</div>
-          <div v-if="userInfo?.national_id && userInfo?.isForeigner">
-            {{ userInfo.national_id }}
-          </div>
-          <div v-else>-</div>
-        </div>
-      </div>
-    </div>
-    <div>
-      <div class="mt-24px text-24px font-bold">{{ $t("現居地址") }}</div>
-      <div class="admApplicantBasicInfoCard">
-        <div style="color: #333333">
-          <div>{{ $t("地址") + $t(":") }}</div>
-          <div>{{ $t("區號") + $t(":") }}</div>
-        </div>
-        <div class="font-bold ml-12px">
-          <div v-if="userInfo?.communicate_address">
-            {{ userInfo?.communicate_address }}
-          </div>
-          <div v-else>-</div>
-          <div v-if="userInfo?.communicate_zipcode">
-            {{ userInfo?.communicate_zipcode }}
-          </div>
-          <div v-else>-</div>
-        </div>
-      </div>
-    </div>
-    <div>
-      <div class="mt-24px text-24px font-bold">{{ $t("身份資料") }}</div>
-      <div class="admApplicantBasicInfoCard">
-        <div style="color: #333333">
-          <div>{{ $t("性別") + $t(":") }}</div>
-          <div>{{ $t("出生國家") + $t(":") }}</div>
-          <div>{{ $t("出生日期") + $t(":") }}</div>
-        </div>
-        <div class="font-bold ml-12px">
-          <div v-if="userInfo?.sex">{{ userInfo?.sex }}</div>
-          <div v-else>-</div>
-          <div v-if="userInfo?.birthcountry">
-            {{ userInfo?.birthcountry }}
-          </div>
-          <div v-else>-</div>
-          <div v-if="userInfo?.birth">{{ userInfo.birth }}</div>
-          <div v-else>-</div>
-        </div>
-      </div>
-    </div>
-    <div>
-      <div class="mt-24px text-24px font-bold">{{ $t("聯絡方式") }}</div>
-      <div class="admApplicantBasicInfoCard">
-        <div style="color: #333333">
-          <div>{{ $t("聯絡號碼") + $t(":") }}</div>
-        </div>
-        <div class="font-bold ml-12px">
-          <div v-if="userInfo?.mobile_phone">
-            {{ userInfo?.mobile_phone }}
-          </div>
-          <div v-else>-</div>
-        </div>
-      </div>
-    </div>
-  </div>
+	<div class="grid">
+		<div>
+			<div class="mt-24px text-24px font-bold">{{ $t("姓名資訊") }}</div>
+			<div class="admApplicantBasicInfoCard">
+				<div style="color: #333333">
+					<div>{{ $t("中文姓氏") + $t(":") }}</div>
+					<div>{{ $t("中文名字") + $t(":") }}</div>
+					<div>{{ $t("英文姓氏") + $t(":") }}</div>
+					<div>{{ $t("英文中間名字") + $t(":") }}</div>
+					<div>{{ $t("英文名字") + $t(":") }}</div>
+				</div>
+				<div class="font-bold ml-12px">
+					<div v-if="userInfo?.cn_surname">
+						{{ userInfo.cn_surname }}
+					</div>
+					<div v-else>-</div>
+					<div v-if="userInfo?.name">{{ userInfo?.name }}</div>
+					<div v-else>-</div>
+					<div v-if="userInfo?.en_surname">
+						{{ userInfo?.en_surname }}
+					</div>
+					<div v-else>-</div>
+					<div v-if="userInfo?.en_midname">
+						{{ userInfo?.en_midname }}
+					</div>
+					<div v-else>-</div>
+					<div v-if="userInfo?.en_givenname">
+						{{ userInfo?.en_givenname }}
+					</div>
+					<div v-else>-</div>
+				</div>
+			</div>
+		</div>
+		<div>
+			<div class="mt-24px text-24px font-bold">
+				{{ $t("入學身分") }}
+			</div>
+			<div class="admApplicantBasicInfoCard">
+				<div style="color: #333333">
+					<div>{{ $t("入學身份") + $t(":") }}</div>
+					<div>{{ $t("國籍") + $t(":") }}</div>
+					<div>{{ $t("身份證字號") + $t(":") }}</div>
+					<div>{{ $t("居留證統一證號") + $t(":") }}</div>
+				</div>
+				<div class="font-bold ml-12px">
+					<div v-if="userInfo?.isForeigner">{{ $t("外籍生") }}</div>
+					<div v-else-if="userInfo?.isForeigner === false">
+						{{ $t("本地生") }}
+					</div>
+					<div v-else>-</div>
+					<div v-if="userInfo?.nationality">
+						{{ userInfo?.nationality }}
+					</div>
+					<div v-else>-</div>
+					<div
+						v-if="
+							userInfo?.national_id &&
+							userInfo?.isForeigner === false
+						"
+					>
+						{{ userInfo.national_id }}
+					</div>
+					<div v-else>-</div>
+					<div v-if="userInfo?.national_id && userInfo?.isForeigner">
+						{{ userInfo.national_id }}
+					</div>
+					<div v-else>-</div>
+				</div>
+			</div>
+		</div>
+		<div>
+			<div class="mt-24px text-24px font-bold">{{ $t("現居地址") }}</div>
+			<div class="admApplicantBasicInfoCard">
+				<div style="color: #333333">
+					<div>{{ $t("地址") + $t(":") }}</div>
+					<div>{{ $t("區號") + $t(":") }}</div>
+				</div>
+				<div class="font-bold ml-12px">
+					<div v-if="userInfo?.communicate_address">
+						{{ userInfo?.communicate_address }}
+					</div>
+					<div v-else>-</div>
+					<div v-if="userInfo?.communicate_zipcode">
+						{{ userInfo?.communicate_zipcode }}
+					</div>
+					<div v-else>-</div>
+				</div>
+			</div>
+		</div>
+		<div>
+			<div class="mt-24px text-24px font-bold">{{ $t("身份資料") }}</div>
+			<div class="admApplicantBasicInfoCard">
+				<div style="color: #333333">
+					<div>{{ $t("性別") + $t(":") }}</div>
+					<div>{{ $t("出生國家") + $t(":") }}</div>
+					<div>{{ $t("出生日期") + $t(":") }}</div>
+				</div>
+				<div class="font-bold ml-12px">
+					<div v-if="userInfo?.sex">{{ userInfo?.sex }}</div>
+					<div v-else>-</div>
+					<div v-if="userInfo?.birthcountry">
+						{{ userInfo?.birthcountry }}
+					</div>
+					<div v-else>-</div>
+					<div v-if="userInfo?.birth">{{ userInfo.birth }}</div>
+					<div v-else>-</div>
+				</div>
+			</div>
+		</div>
+		<div>
+			<div class="mt-24px text-24px font-bold">{{ $t("聯絡方式") }}</div>
+			<div class="admApplicantBasicInfoCard">
+				<div style="color: #333333">
+					<div>{{ $t("聯絡號碼") + $t(":") }}</div>
+				</div>
+				<div class="font-bold ml-12px">
+					<div v-if="userInfo?.mobile_phone">
+						{{ userInfo?.mobile_phone }}
+					</div>
+					<div v-else>-</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script setup lang="ts">
@@ -134,30 +141,30 @@ const props = defineProps(["userId"]);
 const userInfo = ref<AdmAdminGetApplicantInfo>();
 
 useQuery(
-  ["adminApplicantBasicInfo"],
-  async () => {
-    return await api.getApplicantBasicInfo(props.userId);
-  },
-  {
-    onSuccess: (data) => {
-      userInfo.value = data;
-    },
-  }
+	["adminApplicantBasicInfo"],
+	async () => {
+		return await api.getApplicantBasicInfo(props.userId);
+	},
+	{
+		onSuccess: (data) => {
+			userInfo.value = data;
+		},
+	}
 );
 </script>
 
 <style setup lang="css">
 .admApplicantBasicInfoCard {
-  display: flex;
-  margin-top: 16px;
-  padding: 16px 24px;
-  border: 3px dashed rgb(174, 174, 174);
-  border-radius: 16px;
-  background-color: rgb(244, 244, 244);
+	display: flex;
+	margin-top: 16px;
+	padding: 16px 24px;
+	border: 3px dashed rgb(174, 174, 174);
+	border-radius: 16px;
+	background-color: rgb(244, 244, 244);
 }
 .grid {
-  display: grid;
-  grid-template-columns: auto auto;
-  column-gap: 20px;
+	display: grid;
+	grid-template-columns: auto auto;
+	column-gap: 20px;
 }
 </style>
