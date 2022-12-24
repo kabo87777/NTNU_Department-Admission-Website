51b2047a7aeb91ee22548ecaff51944b887ad1e3
flyingdollar.cheng <flyingdollar.cheng@gmail.com>
2022-12-25 03:50:39 +0800

    feat(admissionApplicantSidebar): Layout the page.

--- src/components/sidebars/admissionApplicantSidebar.vue
+++ src/components/sidebars/admissionApplicantSidebar.vue
@@ -1,10 +1,11 @@
 <template>
-	<div class="relative" style="height: 100%">
-		<div class="flex">
-			<div class="sidebarVerticalBigYellowDivider mt-32px"></div>
-			<div class="sidebarVerticalSmallYellowDivider mt-32px"></div>
-			<div class="mt-32px ml-12px w-[100%]">
-				<div class="sidebarTitleBar">
+	<div p="t-8" w="full" h="full" pos="relative">
+		<!-- 當前專案 -->
+		<div flex="~" h="15">
+			<div w="2" mr="1" bg="nGold" />
+			<div w="1" bg="nGold" />
+			<div flex="~ grow" m="x-4" border="1 rounded-lg nGold">
+				<div text="body lg" m="x-2 y-auto">
 					{{
 						$t("admissionApplicantSidebarTitle", {
 							year: currentYear,
@@ -16,299 +17,255 @@
 				</div>
 			</div>
 		</div>
-
-		<router-link
-			:to="{
-				name: 'AdmissionApplicantLatestNews',
-			}"
-			custom
-			v-slot="{ navigate }"
-		>
-			<Button
-				class="p-button-secondary p-button-text !ml-24px !mt-24px !w-[85%] !h-48px !hover:bg-[#ECEDED] !rounded-[8px]"
-				style="{ active: border: 1px solid #736028 !important }"
-				@click="navigate"
-				role="link"
-			>
-				<i class="pi pi-bell" style="font-size: 1rem; color: #101820" />
-				<span
-					class="text-left tracking-3px ml-4 font-bold text-18px text-[#2D2926]"
-				>
-					{{ $t("最新資訊") }}
-				</span>
-			</Button>
-		</router-link>
-
-		<div v-if="isProgramAvailable">
-			<div class="flex mt-32px">
-				<div class="sidebarYellowDivider"></div>
-				<div
-					class="mt-[-8px] ml-8px text-[#736028] text-18px font-bold"
-				>
-					{{ $t("資料填寫、上傳") }}
-				</div>
-			</div>
-
-			<router-link
-				:to="{
-					name: 'AdmissionApplicantBasicInfo',
-				}"
-				custom
-				v-slot="{ navigate }"
-			>
-				<Button
-					class="p-button-secondary p-button-text !ml-24px !mt-16px !w-[85%] !h-48px !hover:bg-[#ECEDED] !rounded-[8px]"
-					@click="navigate"
-					role="link"
-				>
-					<i
-						class="pi pi-user"
-						style="font-size: 1rem; color: #101820"
-					/>
-					<span
-						class="text-left tracking-3px ml-4 font-bold text-18px text-[#2D2926]"
-					>
-						{{ $t("基本資料") }}
-					</span>
-				</Button>
-			</router-link>
-
-			<router-link
-				:to="{
-					name: 'AdmissionApplicantAttachment',
-				}"
-				custom
-				v-slot="{ navigate }"
-			>
-				<Button
-					class="p-button-secondary p-button-text !ml-24px !mt-8px !w-[85%] !h-48px !hover:bg-[#ECEDED] !rounded-[8px]"
-					@click="navigate"
-					role="link"
-				>
-					<i
-						class="pi pi-book"
-						style="font-size: 1rem; color: #101820"
-					/>
-					<span
-						class="text-left tracking-3px ml-4 font-bold text-18px text-[#2D2926]"
-					>
-						{{ $t("附件資料") }}
-					</span>
-				</Button>
-			</router-link>
-
-			<div class="flex mt-32px">
-				<div class="sidebarYellowDivider"></div>
-				<div
-					class="mt-[-8px] ml-8px text-[#736028] text-18px font-bold"
-				>
-					{{ $t("相關作業、系統") }}
-				</div>
-			</div>
-
-			<router-link
-				:to="{
-					name: 'AdmissionApplicantRecommendationLetter',
-				}"
-				custom
-				v-slot="{ navigate }"
-			>
-				<Button
-					class="p-button-secondary p-button-text !ml-24px !mt-16px !w-[85%] !h-48px !hover:bg-[#ECEDED] !rounded-[8px]"
-					@click="navigate"
-					role="link"
-				>
-					<i
-						class="pi pi-envelope"
-						style="font-size: 1rem; color: #101820"
-					/>
-					<span
-						class="text-left tracking-3px ml-4 font-bold text-18px text-[#2D2926]"
-					>
-						{{ $t("推薦信作業") }}
-					</span>
-				</Button>
-			</router-link>
-
-			<router-link
-				:to="{
-					name: 'AdmissionApplicantAdditionalDocs',
-				}"
-				custom
-				v-slot="{ navigate }"
-			>
-				<Button
-					class="p-button-secondary p-button-text !ml-24px !mt-8px !w-[85%] !h-48px !hover:bg-[#ECEDED] !rounded-[8px]"
-					@click="navigate"
-					role="link"
-				>
-					<i
-						class="pi pi-file"
-						style="font-size: 1rem; color: #101820"
-					/>
-					<span
-						class="text-left tracking-3px ml-4 font-bold text-18px text-[#2D2926]"
-					>
-						{{ $t("補交文件系統") }}
-					</span>
-				</Button>
-			</router-link>
-		</div>
+		<!-- 頁面按鈕 -->
 		<div
-			class="bg-gray-200 bg-opacity-50 h-200px w-[100%]"
-			style="
-				position: absolute;
-				bottom: 0px;
-				padding-left: 8px;
-				padding-right: 8px;
-			"
+			h="[calc(100%-14rem)]"
+			overflow="y-auto"
+			scrollbar="thin thumb-nRed-100 thumb-rounded-full"
 		>
-			<div class="mt-16px">
-				<Dialog
-					v-model:visible="isModalVisible"
-					style="width: 500px"
-					:closable="false"
-					:draggable="false"
-					:modal="true"
+			<div flex="~ col gap-6" mt="6">
+				<!-- 1.最新資訊 -->
+				<router-link
+					:to="{
+						name: 'AdmissionApplicantLatestNews',
+					}"
+					custom
+					v-slot="{ navigate }"
 				>
-					<template #header>
-						<div class="font-bold text-[24px]">
-							{{ $t("注意") }}
-						</div>
-					</template>
-					<template #default>
-						<div class="flex">
-							<div class="mt-2px">
-								<i
-									class="pi pi-exclamation-triangle"
-									style="font-size: 20px"
-								/>
-							</div>
-							<div class="ml-4px">
-								{{ $t("確認送出後無法再更改資料") }}
-							</div>
-						</div>
-					</template>
-					<template #footer>
-						<Button
-							class="p-button-outlined p-button-secondary"
-							style="
-								border: 2px solid #a18b4a;
-								color: #736028;
-								height: 36px;
-							"
-							icon="pi pi-times"
-							iconClass="text-[#736028]"
-							:label="$t('取消')"
-							@click="isModalVisible = false"
-						/>
-						<Button
-							class="p-button-outlined p-button-secondary"
-							style="
-								color: #53565a;
-								border: 2px solid #bcd19b;
-								margin-left: 16px;
-								height: 36px;
-							"
-							icon="pi pi-check"
-							iconClass="text-[#53565A]"
-							:label="$t('確認')"
-							:loading="isLoading"
-							@click="sendConfirmation"
-						/>
-					</template>
-				</Dialog>
-				<div class="flex">
-					<div class="m-auto">
-						<Button
-							class="p-button-secondary p-button-outlined"
-							style="height: 40px; border: 2px solid #736028"
-							@click="isModalVisible = true"
-						>
-							<div>
-								<i
-									class="pi pi-check-square text-[#736028]"
-									style="font-size: small"
-								/>
-							</div>
-							<div
-								class="ml-8px font-bold text-15px text-[#736028]"
-							>
-								{{ $t("資料確認送出") }}
-							</div>
-						</Button>
+					<NButton
+						Applicant
+						white
+						icon="pi pi-bell"
+						size="lg"
+						class="py-3 px-6 mx-6 gap-4 !justify-start"
+						:class="letterSpace"
+						@click="navigate"
+						:isSelected="gateway === 'AdmissionApplicantLatestNews'"
+						>{{ $t("最新資訊") }}
+					</NButton>
+				</router-link>
+				<!-- <div v-if="isProgramAvailable"> -->
+				<!-- Divider -->
+				<div flex="~ gap-4" class="items-center" :class="letterSpace">
+					<div w="1/10" h="!1px" bg="nGold" />
+					<div my="auto" text="lg pApplicant">
+						{{ $t("資料填寫、上傳") }}
 					</div>
 				</div>
+				<!-- 2.基本資料 -->
+				<router-link
+					:to="{
+						name: 'AdmissionApplicantBasicInfo',
+					}"
+					custom
+					v-slot="{ navigate }"
+				>
+					<NButton
+						Applicant
+						white
+						icon="pi pi-file-word"
+						size="lg"
+						class="py-3 px-6 mx-6 gap-4 !justify-start"
+						:class="letterSpace"
+						@click="navigate"
+						:isSelected="gateway === 'AdmissionApplicantBasicInfo'"
+						>{{ $t("基本資料") }}
+					</NButton>
+				</router-link>
+				<!-- 3.附件資料 -->
+				<router-link
+					:to="{
+						name: 'AdmissionApplicantAttachment',
+					}"
+					custom
+					v-slot="{ navigate }"
+				>
+					<NButton
+						Applicant
+						white
+						icon="pi pi-file-pdf"
+						size="lg"
+						class="py-3 px-6 mx-6 gap-4 !justify-start"
+						:class="letterSpace"
+						@click="navigate"
+						:isSelected="gateway === 'AdmissionApplicantAttachment'"
+						>{{ $t("附件資料") }}
+					</NButton>
+				</router-link>
+				<!-- Divider -->
+				<div flex="~ gap-4" class="items-center" :class="letterSpace">
+					<div w="1/10" h="!1px" bg="nGold" />
+					<div my="auto" text="lg pApplicant">
+						{{ $t("相關作業、系統") }}
+					</div>
+				</div>
+				<!-- 4.推薦信作業 -->
+				<router-link
+					:to="{
+						name: 'AdmissionApplicantRecommendationLetter',
+					}"
+					custom
+					v-slot="{ navigate }"
+				>
+					<NButton
+						Applicant
+						white
+						icon="pi pi-envelope"
+						size="lg"
+						class="py-3 px-6 mx-6 gap-4 !justify-start"
+						:class="letterSpace"
+						@click="navigate"
+						:isSelected="
+							gateway === 'AdmissionApplicantRecommendationLetter'
+						"
+						>{{ $t("推薦信作業") }}
+					</NButton>
+				</router-link>
+				<!-- 5.補交文件系統 -->
+				<router-link
+					:to="{
+						name: 'AdmissionApplicantAdditionalDocs',
+					}"
+					custom
+					v-slot="{ navigate }"
+				>
+					<NButton
+						Applicant
+						white
+						icon="pi pi-file-export"
+						size="lg"
+						class="py-3 px-6 mx-6 gap-4 !justify-start"
+						:class="letterSpace"
+						@click="navigate"
+						:isSelected="
+							gateway === 'AdmissionApplicantAdditionalDocs'
+						"
+						>{{ $t("補交文件系統") }}
+					</NButton>
+				</router-link>
 			</div>
-			<div class="mt-24px flex relative">
-				<div
-					style="
-						background-color: #8a7b27;
-						border-radius: 50%;
-						width: 40px;
-						height: 40px;
-						margin-top: -4px;
-						margin-left: 16px;
-					"
-				>
-					<i
-						class="pi pi-user ml-6.5px mt-8px"
-						style="font-size: 1.7rem"
-					/>
+		</div>
+		<!-- 頁面底部操作項目 -->
+		<div pos="absolute bottom-0" bg="nGrey-50" w="full">
+			<div flex="~ col gap-6" m="t-4 b-6">
+				<!-- 管理按鈕 -->
+				<div flex="~ gap-4" justify="center">
+					<NButton
+						Applicant
+						@click="isModalVisible = true"
+						icon="pi pi-user-edit"
+						class="p-2 h-11 bg-white whitespace-nowrap"
+						:class="letterSpace"
+						>{{ $t("資料確認送出") }}
+					</NButton>
 				</div>
-				<div class="mt-[-4px] ml-12px">
-					<div
-						class="text-[14px] font-[500] font-bold text-[#736028]"
-					>
-						{{ $t("用戶名稱") }}
+				<!-- 使用者操作 -->
+				<div flex="~ gap-4" mx="6" class="items-center">
+					<!-- Icon -->
+					<div w="10" h="10" bg="nGold-600" border="rounded-full">
+						<i class="pi pi-user" text="!2xl white" mx="2" my="1" />
 					</div>
-					<div
-						class="text-[16px] font-[500] font-bold mt-[8px] tracking-wider"
-					>
-						{{ applicantName }}
-					</div>
-				</div>
-				<div class="absolute right-[0] mt-[-8px]">
-					<router-link
-						:to="{
-							name: 'AdmissionApplicantUserSetting',
-						}"
-						custom
-						v-slot="{ navigate }"
-					>
-						<Button
-							class="p-button-secondary p-button-text p-button-sm"
-							v-tooltip.top="t('使用者管理')"
-							@click="navigate"
-							role="link"
+					<!-- Heading -->
+					<div>
+						<div text="sm pApplicant">{{ $t("申請端帳戶") }}</div>
+						<div
+							text="title"
+							class="whitespace-nowrap"
+							:class="letterSpace"
 						>
-							<img
-								alt="logo"
-								src="/assets/sidebar/Setting_alt_line.png"
-								class="w-18px h-18px"
+							{{ applicantName }}
+						</div>
+					</div>
+					<!-- Button -->
+					<div flex="~ gap-1" ml="auto">
+						<!-- 使用者設定 -->
+						<router-link
+							:to="{
+								name: 'AdmissionApplicantUserSetting',
+							}"
+							custom
+							v-slot="{ navigate }"
+						>
+							<NButton
+								Applicant
+								white
+								icon="pi pi-cog"
+								@click="navigate"
+								size="lg"
+								class="w-12 h-12 m-auto"
+								:isSelected="
+									gateway === 'AdmissionApplicantUserSetting'
+								"
 							/>
-						</Button>
-					</router-link>
-					<Button
-						class="p-button-secondary p-button-text p-button-sm"
-						v-tooltip.top="t('登出')"
-						@click="signOut"
-					>
-						<img
-							alt="logo"
-							src="/assets/sidebar/Sign_out_circle.png"
-							class="w-18px h-18px"
+						</router-link>
+						<!-- 登出 -->
+						<NButton
+							Applicant
+							white
+							icon="pi pi-sign-out"
+							size="lg"
+							class="w-12 h-12 m-auto"
+							@click="signOut"
 						/>
-					</Button>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
+	<Dialog
+		v-model:visible="isModalVisible"
+		class="!max-w-120"
+		:closable="false"
+		:draggable="false"
+		:modal="true"
+	>
+		<template #header>
+			<div text="xl title" font="medium">
+				{{ $t("資料確認送出") }}
+			</div>
+		</template>
+		<template #default>
+			<div class="flex items-center mx-4 gap-6">
+				<i
+					class="pi pi-exclamation-triangle"
+					text="warning"
+					style="font-size: 4rem"
+				/>
+				<div>
+					{{
+						$t(
+							"請注意，資料一經送出後即「無法」再次更改資料。送出前請務必確認資料無誤！"
+						)
+					}}
+				</div>
+			</div>
+		</template>
+		<template #footer>
+			<div flex="~ gap-6" justify="center">
+				<NButton
+					Success
+					icon="pi pi-check"
+					class="h-11 min-w-32"
+					:loading="isLoading"
+					@click="sendConfirmation"
+				>
+					{{ $t("送出") }}
+				</NButton>
+				<NButton
+					icon="pi pi-times"
+					class="h-11 min-w-32"
+					@click="isModalVisible = false"
+				>
+					{{ $t("取消") }}
+				</NButton>
+			</div>
+		</template>
+	</Dialog>
 </template>
 
 <script setup lang="ts">
 import { computed, toRaw, ref, reactive } from "vue";
-import { useRouter } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { useQuery } from "@tanstack/vue-query";
 import { useI18n } from "vue-i18n";
 import dayjs from "dayjs";
@@ -318,17 +275,20 @@ import { AdmissionApplicantAPI } from "@/api/admission/applicant/api";
 
 import "primeicons/primeicons.css";
 import "primevue/resources/primevue.min.css";
-import Button from "primevue/button";
+import NButton from "@/styles/CustomButton.vue";
 import Dialog from "primevue/dialog";
 import { useToast } from "primevue/usetoast";
 
 import "@/styles/customize.css";
 
 const router = useRouter();
+const route = useRoute();
+const gateway = computed(() => route.name);
 const applicantAuth = useAdmissionApplicantAuthStore();
 const applicantName = window.localStorage.getItem("AdmissionApplicantUsername");
 
 const { t } = useI18n();
+const { locale } = useI18n();
 const toast = useToast();
 const isModalVisible = ref(false);
 const isLoading = ref(false);
@@ -342,6 +302,12 @@ const rocYear = currentYear - 1911;
 let fetchResponse = reactive({
 	success: false,
 	message: "" as string | [],
+});
+
+const letterSpace = computed(() => {
+	if (locale.value == "zh") return "tracking-[.125em]";
+	if (locale.value == "en") return "tracking-tighter";
+	else return "";
 });
 
 const sendConfirmation = async () => {
