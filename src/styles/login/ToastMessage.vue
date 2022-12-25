612693b035d10e4174e092175b57018dadcf8571
SubaRya <index20010928@gmail.com>
2022-12-25 19:20:54 +0800

    feat: add confirmation of activate account or not

--- src/styles/login/ToastMessage.vue
+++ src/styles/login/ToastMessage.vue
@@ -10,6 +10,22 @@
 			</div>
 		</div>
 	</div>
+	<div v-if="props.type === 'ConfirmSuccess'" flex="~ gap-4">
+		<i class="pi pi-check" style="font-size: 2rem" />
+		<div space="y-2">
+			<div text="xl" font="medium">
+				成功啟動帳號 Activate Account Successfully
+			</div>
+		</div>
+	</div>
+	<div v-if="props.type === 'ConfirmFailed'" flex="~ gap-4" text="danger">
+		<i class="pi pi-ban" style="font-size: 2rem" />
+		<div space="y-2">
+			<div text="xl" font="medium">
+				啟動帳號失敗 Failed to Activate Account
+			</div>
+		</div>
+	</div>
 	<!-- Password Reset Failure -->
 	<!-- <div v-if="props.type === 'resetFailure'" flex="~ gap-4" text="danger">
 		<i class="pi pi-ban" style="font-size: 2rem" />
