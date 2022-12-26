6ddafd3e539c5f1b367b4b1323f64cb3e34872df
Howard Guo <4442713-toto6038@users.noreply.gitlab.com>
2022-12-26 19:20:34 +0800

    chore: remove edit applicant button for recruitment admin

--- src/views/recruitment/manager/ListApplicant.vue
+++ src/views/recruitment/manager/ListApplicant.vue
@@ -25,19 +25,12 @@
 					<template #header>{{ $t("操作") }}</template>
 					<template #body="slotProps">
 						<span class="flex gap-4">
-							<!-- Edit button -->
-							<NButton
-								Admin
-								icon="pi pi-pencil"
-								class="p-2"
-								@click="editModal.open(slotProps.data)"
-							/>
 							<!-- Delete button -->
 							<NButton
 								Danger
 								icon="pi pi-trash"
 								class="p-2"
-								@click="actions.delete(slotProps.data.id)"
+								@click="confirmDeleteAccount(slotProps.data.id)"
 							/>
 						</span>
 					</template>
@@ -51,80 +44,6 @@
 			</DataTable>
 		</template>
 	</Layout>
-	<Dialog
-		v-model:visible="editModal.visible"
-		:modal="true"
-		:draggable="false"
-		:closable="false"
-		class="min-w-120"
-	>
-		<template #header>
-			<div text="xl title">
-				{{ $t("編輯申請者帳號") }}
-			</div>
-		</template>
-
-		<template #default>
-			<div flex="~ col gap-6" w="full">
-				<div space="y-1">
-					<div text="body">{{ $t("帳號 ID") }}</div>
-					<div>{{ editModal.data.id }}</div>
-				</div>
-				<div space="y-1">
-					<div text="body">{{ $t("姓名") }}</div>
-					<InputText
-						type="text"
-						class="w-full mt-1"
-						v-model:model-value="editModal.data.name"
-						disabled="true"
-					/>
-				</div>
-				<div space="y-1">
-					<div text="body">{{ $t("電子信箱") }}</div>
-					<InputText
-						type="email"
-						class="w-full mt-1"
-						v-model:model-value="editModal.data.email"
-						:disabled="true"
-					/>
-				</div>
-				<div space="y-1">
-					<div text="body">{{ $t("密碼") }}</div>
-					<Password
-						class="w-full"
-						v-model="editModal.data.password"
-						:feedback="false"
-						:disabled="
-							editModal.data.allow_password_change === false
-						"
-						:toggle-mask="
-							editModal.data.allow_password_change === true
-						"
-						@focus.once="editModal.data.password = ''"
-						input-class="w-full"
-					/>
-				</div>
-			</div>
-		</template>
-
-		<template #footer>
-			<div class="flex justify-center gap-6">
-				<NButton
-					Success
-					icon="pi pi-check"
-					class="h-11 min-w-36"
-					@click="editModal.submit"
-					>{{ $t("儲存") }}
-				</NButton>
-				<NButton
-					icon="pi pi-times"
-					class="h-11 min-w-36"
-					@click="editModal.visible = false"
-					>{{ $t("取消") }}
-				</NButton>
-			</div>
-		</template>
-	</Dialog>
 	<ConfirmDialog :draggable="false" />
 </template>
 
@@ -147,7 +66,6 @@ import { RecruitmentAdminApplicantsListResponse } from "@/api/recruitment/admin/
 import { useGlobalStore } from "@/stores/RecruitmentAdminStore";
 import ConfirmDialog from "primevue/confirmdialog";
 import { useConfirm } from "primevue/useconfirm";
-
 import { useI18n } from "vue-i18n";
 import { useToast } from "primevue/usetoast";
 
@@ -193,14 +111,6 @@ const {
 const isProcessing = ref(false);
 const isTableLoading = computed(() => isLoading.value || isProcessing.value);
 
-interface modalFields {
-	id: number;
-	name: string;
-	email: string;
-	password: string;
-	allow_password_change: boolean;
-}
-
 store.$subscribe((mutation, state) => {
 	if (state.program) {
 		console.log(state.program.category + state.program.name);
@@ -208,68 +118,18 @@ store.$subscribe((mutation, state) => {
 	refetch();
 });
 
-class Modal {
-	visible: boolean;
-	data: modalFields;
-
-	constructor() {
-		this.visible = false;
-		this.data = {
-			id: -1,
-			name: "",
-			email: "",
-			password: "",
-			allow_password_change: false,
-		};
-	}
-
-	show() {
-		this.visible = true;
-	}
-
-	open(applicantData: RecruitmentAdminApplicantsListResponse) {
-		this.data = (({
-			id,
-			name,
-			email,
-			allow_password_change,
-		}: RecruitmentAdminApplicantsListResponse) => ({
-			id,
-			name,
-			email,
-			allow_password_change,
-			// This is used to show dots as visual hint for user
-			// After user click the password input box, the value will be cleared
-			password: "JUST A PLACEHOLDER",
-		}))(applicantData);
-		this.show();
-	}
-	close() {
-		this.visible = false;
-	}
-
-	submit() {
-		this.close();
-	}
-}
-
-const editModal = ref(new Modal());
-
-const actions = {
-	edit: editModal.value.open,
-	delete: (id: number) => {
-		console.log("Confirm");
-		confirm.require({
-			message: $t("此動作無法回復"),
-			header: $t("是否要刪除此帳號？"),
-			icon: "pi pi-exclamation-triangle",
-			acceptLabel: $t("確認"),
-			rejectLabel: $t("取消"),
-			accept: () => {
-				deleteApplicantAPI(id);
-			},
-		});
-	},
+const confirmDeleteAccount = (id: number) => {
+	console.log("Confirm");
+	confirm.require({
+		message: $t("此動作無法回復"),
+		header: $t("是否要刪除此帳號？"),
+		icon: "pi pi-exclamation-triangle",
+		acceptLabel: $t("確認"),
+		rejectLabel: $t("取消"),
+		accept: () => {
+			deleteApplicantAPI(id);
+		},
+	});
 };
 
 const getTableItemQty = computed(() => {
